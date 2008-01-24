bfa9234906a166f8e57e6892236080d70eb8f2cd
Unknown TFS user <unknown@tfs.local>
2008-01-24 00:11:58 +0000

    Disable the global catch in the debug build.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C15116

--- Program.cs
+++ Program.cs
@@ -12,8 +12,10 @@ namespace Rawr
 		[STAThread]
 		static void Main()
 		{
+#if !DEBUG 
 			try
 			{
+#endif //DEBUG
 				bool bAppFirstInstance;
 				System.Threading.Mutex oMutex = new System.Threading.Mutex(true, "Global\\Rawr", out bAppFirstInstance);
 				if (bAppFirstInstance)
@@ -22,13 +24,16 @@ namespace Rawr
 					Application.SetCompatibleTextRenderingDefault(false);
 					Application.Run(new FormMain());
 				}
+                
+#if !DEBUG 
 			}
 			catch (Exception ex)
 			{
 				MessageBox.Show("Rawr encountered a serious error. Please copy and paste this into an e-mail to cnervig@hotmail.com. Thanks!\r\n\r\n\r\n" + ex.Message + "\r\n\r\n" + ex.StackTrace);
 			}
 			Log.Close();
-		} 
+#endif //DEBUG
+        } 
     }
 }
 
