1273a7a4596723d7a15943e0923217a0a76f4071
Unknown TFS user <unknown@tfs.local>
2008-03-06 02:30:29 +0000

    Ret - Fixed bugs with persistence of manually entered Talents
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C15855

--- Rawr.Retribution/Talents.cs
+++ Rawr.Retribution/Talents.cs
@@ -30,17 +30,17 @@ namespace Rawr.Retribution
         {
             if (Character.CalculationOptions.ContainsKey("TalentsSaved") && Character.CalculationOptions["TalentsSaved"] == "1")
             {
-                comboBoxTwoHandedSpec.SelectedItem = Character.CalculationOptions["TwoHandedSpec"];
-                comboBoxConviction.SelectedItem =Character.CalculationOptions["Conviction"];
-                comboBoxCrusade.SelectedItem =Character.CalculationOptions["Crusade"];
-                comboBoxDivineStrength.SelectedItem = Character.CalculationOptions["DivineStrength"];
-                comboBoxFanaticism.SelectedItem = Character.CalculationOptions["Fanaticism"];
-                comboBoxImprovedSanctityAura.SelectedItem = Character.CalculationOptions["ImprovedSanctityAura"];
-                comboBoxPrecision.SelectedItem = Character.CalculationOptions["Precision"];
-                comboBoxSanctityAura.SelectedItem = Character.CalculationOptions["SanctityAura"];
-                comboBoxSanctifiedSeals.SelectedItem = Character.CalculationOptions["SanctifiedSeals"];
-                comboBoxVengeance.SelectedItem = Character.CalculationOptions["Vengeance"];
-                
+                comboBoxTwoHandedSpec.SelectedItem =(Character.CalculationOptions.ContainsKey("TwoHandedSpec") ? Character.CalculationOptions["TwoHandedSpec"] : "0");
+                comboBoxConviction.SelectedItem = (Character.CalculationOptions.ContainsKey("Conviction") ? Character.CalculationOptions["Conviction"] : "0");
+                comboBoxCrusade.SelectedItem = (Character.CalculationOptions.ContainsKey("Crusade") ? Character.CalculationOptions["Crusade"] : "0");
+                comboBoxDivineStrength.SelectedItem = (Character.CalculationOptions.ContainsKey("DivineStrength") ? Character.CalculationOptions["DivineStrength"] : "0");
+                comboBoxFanaticism.SelectedItem = (Character.CalculationOptions.ContainsKey("Fanaticism") ? Character.CalculationOptions["Fanaticism"] : "0");
+                comboBoxImprovedSanctityAura.SelectedItem = (Character.CalculationOptions.ContainsKey("ImprovedSanctityAura") ? Character.CalculationOptions["ImprovedSanctityAura"] : "0");
+                comboBoxPrecision.SelectedItem = (Character.CalculationOptions.ContainsKey("Precision") ? Character.CalculationOptions["Precision"] : "0");
+                comboBoxSanctityAura.SelectedItem = (Character.CalculationOptions.ContainsKey("SanctityAura") ? Character.CalculationOptions["SanctityAura"] : "0");
+                comboBoxSanctifiedSeals.SelectedItem = (Character.CalculationOptions.ContainsKey("SanctifiedSeals") ? Character.CalculationOptions["SanctifiedSeals"] : "0");
+                comboBoxVengeance.SelectedItem = (Character.CalculationOptions.ContainsKey("Vengeance") ? Character.CalculationOptions["Vengeance"] : "0");
+                               
             }
         }
         private void comboBox_SelectedIndexChanged(object sender, EventArgs e)
@@ -49,6 +49,7 @@ namespace Rawr.Retribution
             string talent = cb.Name.Substring(8);
             Character.CalculationOptions[talent] = cb.SelectedItem.ToString();
             Character.OnItemsChanged();
+            Character.CalculationOptions["TalentsSaved"] = "1";
         }
         
     }
