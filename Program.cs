e77c460a62444e1f9477773ea2fc7b1cced6ed9a
Kavan_cp <673bc13b9e52e766925399323aa7254450550230hcsPpcdD>
2008-03-07 02:26:11 +0000

    Disable top level exception catching in debug builds.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C15901

--- Program.cs
+++ Program.cs
@@ -44,11 +44,13 @@ namespace Rawr
 				//log.Error("Type Load Exception", ex);
                 MessageBox.Show(ex.Message, "when cat durid is FITE do not ask for HEEL and NINIRVATE!"); //Heh
             }
+#if !DEBUG
             catch (Exception ex)
             {
-				//log.Error("Top Level exception caught", ex);
+				log.Error("Top Level exception caught", ex);
                 //MessageBox.Show("Rawr encountered a serious error. Please copy and paste this into an e-mail to cnervig@hotmail.com. Thanks!\r\n\r\n\r\n" + ex.Message + "\r\n\r\n" + ex.StackTrace);
             }
+#endif
 		}
 
 		//private static void RawrCatIntro()
