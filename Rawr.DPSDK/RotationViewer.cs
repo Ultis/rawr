d4c60cb54db731473f90bac67c1fd30062298e19
Unknown TFS user <unknown@tfs.local>
2008-11-06 01:03:23 +0000

    dpsDK updates
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C23940

--- Rawr.DPSDK/RotationViewer.cs
+++ Rawr.DPSDK/RotationViewer.cs
@@ -17,6 +17,20 @@ namespace Rawr.DPSDK
         {
             InitializeComponent();
             rotation = r;
+
+            txtBS.KeyPress += new KeyPressEventHandler(txtBS_KeyPress);
+            txtDC.KeyPress += new KeyPressEventHandler(txtDC_KeyPress);
+            txtDuration.KeyPress += new KeyPressEventHandler(txtDuration_KeyPress);
+            txtFS.KeyPress += new KeyPressEventHandler(txtFS_KeyPress);
+            txtHB.KeyPress += new KeyPressEventHandler(txtHB_KeyPress);
+            txtHS.KeyPress += new KeyPressEventHandler(txtHS_KeyPress);
+            txtIT.KeyPress += new KeyPressEventHandler(txtIT_KeyPress);
+            txtNumDisease.KeyPress += new KeyPressEventHandler(txtNumDisease_KeyPress);
+            txtOblit.KeyPress += new KeyPressEventHandler(txtOblit_KeyPress);
+            txtPS.KeyPress += new KeyPressEventHandler(txtPS_KeyPress);
+            txtSS.KeyPress += new KeyPressEventHandler(txtSS_KeyPress);
+            txtUB.KeyPress += new KeyPressEventHandler(txtUB_KeyPress);
+            txtUptime.KeyPress += new KeyPressEventHandler(txtUptime_KeyPress);
         }
 
         private void loadCurRotation()
@@ -38,19 +52,19 @@ namespace Rawr.DPSDK
 
         private void lockTxt()
         {
-            txtBS.ReadOnly = true; 
-            txtDC.ReadOnly = true; 
-            txtDuration.ReadOnly = true; 
-            txtFS.ReadOnly = true; 
+            txtBS.ReadOnly = true;
+            txtDC.ReadOnly = true;
+            txtDuration.ReadOnly = true;
+            txtFS.ReadOnly = true;
             txtHB.ReadOnly = true;
-            txtHS.ReadOnly = true; 
-            txtIT.ReadOnly = true; 
-            txtNumDisease.ReadOnly = true; 
-            txtOblit.ReadOnly = true; 
-            txtPS.ReadOnly = true; 
-            txtSS.ReadOnly = true; 
-            txtUB.ReadOnly = true; 
-            txtUptime.ReadOnly = true; 
+            txtHS.ReadOnly = true;
+            txtIT.ReadOnly = true;
+            txtNumDisease.ReadOnly = true;
+            txtOblit.ReadOnly = true;
+            txtPS.ReadOnly = true;
+            txtSS.ReadOnly = true;
+            txtUB.ReadOnly = true;
+            txtUptime.ReadOnly = true;
         }
 
         private void unLockTxt()
@@ -67,7 +81,7 @@ namespace Rawr.DPSDK
             txtPS.ReadOnly = false;
             txtSS.ReadOnly = false;
             txtUB.ReadOnly = false;
-            txtUptime.ReadOnly = false; 
+            txtUptime.ReadOnly = false;
         }
 
         private void RotationViewer_Load(object sender, EventArgs e)
@@ -127,6 +141,203 @@ namespace Rawr.DPSDK
                 lockTxt();
                 rotation.setRotation(Rotation.Type.Blood);
                 loadCurRotation();
+            }
+        }
+
+        //text changes, commits VALID entries into memory
+
+        void txtDC_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            try
+            {
+                double d = double.Parse(t.Text);
+                float f = (float)d;
+                rotation.DeathCoil = f;
+            }
+            catch
+            {
+                t.Text = "";
+            }
+        }
+
+        void txtBS_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            try
+            {
+                double d = double.Parse(t.Text);
+                float f = (float)d;
+                rotation.BloodStrike = f;
+            }
+            catch
+            {
+                t.Text = "";
+            }
+        }
+
+        void txtDuration_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            try
+            {
+                double d = double.Parse(t.Text);
+                float f = (float)d;
+                rotation.curRotationDuration = f;
+            }
+            catch
+            {
+                t.Text = "";
+            }
+        }
+
+        void txtFS_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            try
+            {
+                double d = double.Parse(t.Text);
+                float f = (float)d;
+                rotation.FrostStrike = f;
+            }
+            catch
+            {
+                t.Text = "";
+            }
+        }
+
+        void txtHB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            try
+            {
+                double d = double.Parse(t.Text);
+                float f = (float)d;
+                rotation.HowlingBlast = f;
+            }
+            catch
+            {
+                t.Text = "";
+            }
+        }
+
+        void txtHS_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            try
+            {
+                double d = double.Parse(t.Text);
+                float f = (float)d;
+                rotation.HeartStrike = f;
+            }
+            catch
+            {
+                t.Text = "";
+            }
+        }
+
+        void txtIT_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            try
+            {
+                double d = double.Parse(t.Text);
+                float f = (float)d;
+                rotation.IcyTouch = f;
+            }
+            catch
+            {
+                t.Text = "";
+            }
+        }
+
+        void txtNumDisease_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            try
+            {
+                double d = double.Parse(t.Text);
+                float f = (float)d;
+                rotation.numDisease = f;
+            }
+            catch
+            {
+                t.Text = "";
+            }
+        }
+
+        void txtOblit_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            try
+            {
+                double d = double.Parse(t.Text);
+                float f = (float)d;
+                rotation.Obliterate = f;
+            }
+            catch
+            {
+                t.Text = "";
+            }
+        }
+
+        void txtPS_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            try
+            {
+                double d = double.Parse(t.Text);
+                float f = (float)d;
+                rotation.PlagueStrike = f;
+            }
+            catch
+            {
+                t.Text = "";
+            }
+        }
+
+        void txtSS_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            try
+            {
+                double d = double.Parse(t.Text);
+                float f = (float)d;
+                rotation.ScourgeStrike = f;
+            }
+            catch
+            {
+                t.Text = "";
+            }
+        }
+
+        void txtUB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            try
+            {
+                double d = double.Parse(t.Text);
+                float f = (float)d;
+                rotation.UnholyBlight = f;
+            }
+            catch
+            {
+                t.Text = "";
+            }
+        }
+
+        void txtUptime_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            try
+            {
+                double d = double.Parse(t.Text);
+                float f = (float)d;
+                rotation.diseaseUptime = f;
+            }
+            catch
+            {
+                t.Text = "";
             }
         }
     }
