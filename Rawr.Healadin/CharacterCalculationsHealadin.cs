07a7476878242599d6c3fab98c25d3c90bc6563a
Ermad_cp <f0f72a736fc87c6bddb1ad1c9da8193512a0ac44B8bybe4x>
2008-11-07 06:12:04 +0000

    Healadin: Some more formatting of calculations display. Sorry for the slew or checkins :(.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C23980

--- Rawr.Healadin/CharacterCalculationsHealadin.cs
+++ Rawr.Healadin/CharacterCalculationsHealadin.cs
@@ -92,54 +92,54 @@ namespace Rawr.Healadin
         {
             Dictionary<string, string> dictValues = new Dictionary<string, string>();
             //Basic Stats
-            dictValues.Add("Health", BasicStats.Health.ToString());
-            dictValues.Add("Stamina", BasicStats.Stamina.ToString());
-            dictValues.Add("Mana", BasicStats.Mana.ToString());
-            dictValues.Add("Intellect", BasicStats.Intellect.ToString());
-            dictValues.Add("Spell Power", BasicStats.SpellPower.ToString());
-            dictValues.Add("Mp5", BasicStats.Mp5.ToString());
+            dictValues.Add("Health", BasicStats.Health.ToString("N00"));
+            dictValues.Add("Stamina", BasicStats.Stamina.ToString("N00"));
+            dictValues.Add("Mana", BasicStats.Mana.ToString("N00"));
+            dictValues.Add("Intellect", BasicStats.Intellect.ToString("N00"));
+            dictValues.Add("Spell Power", BasicStats.SpellPower.ToString("N00"));
+            dictValues.Add("Mp5", BasicStats.Mp5.ToString("N00"));
             dictValues.Add("Spell Crit", string.Format("{0}%*{1} Crit Rating", Math.Round(BasicStats.SpellCrit * 100, 2), BasicStats.CritRating));
             dictValues.Add("Spell Haste", string.Format("{0}%*{1} Haste Rating", Math.Round(BasicStats.SpellHaste * 100, 2), BasicStats.HasteRating));
             // Cycle Stats
-            dictValues.Add("Total Healed", Math.Round(TotalHealed).ToString());
-            dictValues.Add("Total Mana", Math.Round(TotalMana).ToString());
-            dictValues.Add("Average Hps", Math.Round(AvgHPS).ToString());
-            dictValues.Add("Average Hpm", Math.Round(AvgHPM, 2).ToString());
+            dictValues.Add("Total Healed", string.Format("{0} healing", Math.Round(TotalHealed).ToString("N00")));
+            dictValues.Add("Total Mana", string.Format("{0} mana", Math.Round(TotalMana).ToString("N00")));
+            dictValues.Add("Average Healing per sec", string.Format("{0} hps", Math.Round(AvgHPS).ToString("N00")));
+            dictValues.Add("Average Healing per mana", string.Format("{0} hpm", Math.Round(AvgHPM, 2).ToString()));
             // Flash of Light
-            dictValues.Add("FoL Average Heal", string.Format("{0}", Math.Round(FoLAvgHeal)));
+            dictValues.Add("FoL Average Heal", string.Format("{0} healing", Math.Round(FoLAvgHeal).ToString("N00")));
             dictValues.Add("FoL Crit", string.Format("{0}%", Math.Round(FoLCrit * 100, 2)));
             dictValues.Add("FoL Cast Time", string.Format("{0} sec", Math.Round(FoLCastTime, 2)));
             dictValues.Add("FoL Healing per sec", string.Format("{0} hps", Math.Round(FoLHPS)));
             dictValues.Add("FoL Healing per mana", string.Format("{0} hpm", Math.Round(FoLHPM, 2)));
             dictValues.Add("FoL Rotation Time", string.Format("{0} sec", Math.Round(FoLTime, 2)));
-            dictValues.Add("FoL Healed", Math.Round(FoLHealed).ToString("N00"));
-            dictValues.Add("FoL Mana Usage", string.Format("{0} mana", Math.Round(FoLUsage)));
+            dictValues.Add("FoL Healed", string.Format("{0} healing", Math.Round(FoLHealed).ToString("N00")));
+            dictValues.Add("FoL Mana Usage", string.Format("{0} mana", Math.Round(FoLUsage).ToString("N00")));
             // Holy Light
-            dictValues.Add("HL Average Heal", string.Format("{0}", Math.Round(HLAvgHeal)));
+            dictValues.Add("HL Average Heal", string.Format("{0} healing", Math.Round(HLAvgHeal).ToString("N00")));
             dictValues.Add("HL Crit", string.Format("{0}%", Math.Round(HLCrit * 100, 2)));
             dictValues.Add("HL Cast Time", string.Format("{0} sec", Math.Round(HLCastTime, 2)));
             dictValues.Add("HL Healing per sec", string.Format("{0} hps", Math.Round(HLHPS)));
             dictValues.Add("HL Healing per mana", string.Format("{0} hpm", Math.Round(HLHPM, 2)));
             dictValues.Add("HL Rotation Time", string.Format("{0} sec", Math.Round(HLTime, 2)));
-            dictValues.Add("HL Healed", Math.Round(HLHealed).ToString("N00"));
-            dictValues.Add("HL Mana Usage", string.Format("{0} mana", Math.Round(HLUsage)));
+            dictValues.Add("HL Healed",  string.Format("{0} healing", Math.Round(HLHealed).ToString("N00")));
+            dictValues.Add("HL Mana Usage", string.Format("{0} mana", Math.Round(HLUsage).ToString("N00")));
             // Holy Shock
-            dictValues.Add("HS Average Heal", string.Format("{0}", Math.Round(HSAvgHeal)));
+            dictValues.Add("HS Average Heal", string.Format("{0} healing", Math.Round(HSAvgHeal).ToString("N00")));
             dictValues.Add("HS Crit", string.Format("{0}%", Math.Round(HSCrit * 100, 2)));
             dictValues.Add("HS Cast Time", string.Format("{0} sec", Math.Round(HSCastTime, 2)));
             dictValues.Add("HS Healing per sec", string.Format("{0} hps", Math.Round(HSHPS)));
             dictValues.Add("HS Healing per mana", string.Format("{0} hpm", Math.Round(HSHPM, 2)));
             dictValues.Add("HS Rotation Time", string.Format("{0} sec", Math.Round(HSTime, 2)));
-            dictValues.Add("HS Healed", Math.Round(HSHealed).ToString("N00"));
-            dictValues.Add("HS Mana Usage", string.Format("{0} mana", Math.Round(HSUsage)));
+            dictValues.Add("HS Healed",  string.Format("{0} healing", Math.Round(HSHealed).ToString("N00")));
+            dictValues.Add("HS Mana Usage", string.Format("{0} mana", Math.Round(HSUsage).ToString("N00")));
             // Beacon of Light
-            dictValues.Add("BoL Healed", Math.Round(BoLHealed).ToString("N00"));
+            dictValues.Add("BoL Healed",  string.Format("{0} healing", Math.Round(BoLHealed).ToString("N00")));
             dictValues.Add("BoL Casts", string.Format("{0} gcds", Math.Round(BoLCasts)));
-            dictValues.Add("BoL Mana Usage", string.Format("{0} mana", Math.Round(BoLUsage)));
+            dictValues.Add("BoL Mana Usage", string.Format("{0} mana", Math.Round(BoLUsage).ToString("N00")));
             // Judgement
-            dictValues.Add("JotP Effective Haste", string.Format("{0}%", Math.Round(JotPHaste * 100, 2)));
+            dictValues.Add("JotP Effective Haste", string.Format("{0}% haste", Math.Round(JotPHaste * 100, 2)));
             dictValues.Add("JotP Casts", string.Format("{0} gcds", Math.Round(JotPCasts)));
-            dictValues.Add("JotP Mana Usage", string.Format("{0} mana", Math.Round(JotPUsage)));
+            dictValues.Add("JotP Mana Usage", string.Format("{0} mana", Math.Round(JotPUsage).ToString("N00")));
 
             return dictValues;
         }
