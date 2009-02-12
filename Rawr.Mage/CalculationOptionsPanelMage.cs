2d780af8fb3a4e2740d5edb1ca149a581c2cb93c
Kavan_cp <673bc13b9e52e766925399323aa7254450550230hcsPpcdD>
2009-02-12 10:23:29 +0000

    Changed optimal arcane cycle computation to include both base cycles and with arcane power.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C31321

--- Rawr.Mage/CalculationOptionsPanelMage.cs
+++ Rawr.Mage/CalculationOptionsPanelMage.cs
@@ -84,35 +84,59 @@ namespace Rawr.Mage
             calculationResult.Character = Character;
             calculationResult.CalculationOptions = calculationOptions;
             CastingState baseState = new CastingState(calculationResult, characterStats, calculationOptions, armor, Character, false, false, false, false, false, false, false, false, false, false, false, false, false, false);
+            CastingState apState = new CastingState(calculationResult, characterStats, calculationOptions, armor, Character, true, false, false, false, false, false, false, false, false, false, false, false, false, false);
 
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Optimal Cycle Palette:");
+            sb.AppendLine("");
+            sb.AppendLine("Base:");
+
+            ComputeOptimalCycles(baseState, sb);
+
+            sb.AppendLine("");
+            sb.AppendLine("Arcane Power:");
+
+            ComputeOptimalCycles(apState, sb);
+
+            MessageBox.Show(sb.ToString());
+
+
+            /*foreach (GenericArcane cycle in cycleDict.Values)
+            {
+                System.Diagnostics.Debug.Write(cycle.Name + ":\r\n" + cycle.DamagePerSecond + " dps\r\n" + cycle.ManaPerSecond + " mps\r\n" + cycle.SpellDistribution + "\r\n");
+            }*/
+        }
+
+        private static StringBuilder ComputeOptimalCycles(CastingState baseState, StringBuilder sb)
+        {
             Dictionary<string, GenericArcane> cycleDict = new Dictionary<string, GenericArcane>();
             double[] spellControl = new double[46];
             for (int control0 = 0; control0 < 3; control0++)
-            for (int control1 = 0; control1 < 3; control1++)
-            for (int control2 = 0; control2 < 2; control2++)
-            for (int control3 = 0; control3 < 2; control3++)
-            for (int control4 = 0; control4 < 3; control4++)
-            for (int control5 = 0; control5 < 3; control5++)
-            for (int control6 = 0; control6 < 3; control6++)
-            for (int control7 = 0; control7 < 3; control7++)
-            for (int control8 = 0; control8 < 3; control8++)
-            {
-                for (int i = 0; i < 46; i++) spellControl[i] = 0;
-                spellControl[0 + control0] = 1;
-                spellControl[5 + control1] = 1;
-                spellControl[11 + control2] = 1;
-                spellControl[15 + control3] = 1;
-                spellControl[19 + control4] = 1;
-                spellControl[24 + control5] = 1;
-                spellControl[30 + control6] = 1;
-                spellControl[35 + control7] = 1;
-                spellControl[41 + control8] = 1;
-                GenericArcane generic = new GenericArcane(control0.ToString() + control1.ToString() + control2.ToString() + control3.ToString() + control4.ToString() + control5.ToString() + control6.ToString() + control7.ToString() + control8.ToString(), baseState, spellControl[0], spellControl[1], spellControl[2], spellControl[3], spellControl[4], spellControl[5], spellControl[6], spellControl[7], spellControl[8], spellControl[9], spellControl[10], spellControl[11], spellControl[12], spellControl[13], spellControl[14], spellControl[15], spellControl[16], spellControl[17], spellControl[18], spellControl[19], spellControl[20], spellControl[21], spellControl[22], spellControl[23], spellControl[24], spellControl[25], spellControl[26], spellControl[27], spellControl[28], spellControl[29], spellControl[30], spellControl[31], spellControl[32], spellControl[33], spellControl[34], spellControl[35], spellControl[36], spellControl[37], spellControl[38], spellControl[39], spellControl[40], spellControl[41], spellControl[42], spellControl[43], spellControl[44], spellControl[45]);
-                if (!cycleDict.ContainsKey(generic.SpellDistribution))
-                {
-                    cycleDict.Add(generic.SpellDistribution, generic);
-                }
-            }
+                for (int control1 = 0; control1 < 3; control1++)
+                    for (int control2 = 0; control2 < 2; control2++)
+                        for (int control3 = 0; control3 < 2; control3++)
+                            for (int control4 = 0; control4 < 3; control4++)
+                                for (int control5 = 0; control5 < 3; control5++)
+                                    for (int control6 = 0; control6 < 3; control6++)
+                                        for (int control7 = 0; control7 < 3; control7++)
+                                            for (int control8 = 0; control8 < 3; control8++)
+                                            {
+                                                for (int i = 0; i < 46; i++) spellControl[i] = 0;
+                                                spellControl[0 + control0] = 1;
+                                                spellControl[5 + control1] = 1;
+                                                spellControl[11 + control2] = 1;
+                                                spellControl[15 + control3] = 1;
+                                                spellControl[19 + control4] = 1;
+                                                spellControl[24 + control5] = 1;
+                                                spellControl[30 + control6] = 1;
+                                                spellControl[35 + control7] = 1;
+                                                spellControl[41 + control8] = 1;
+                                                GenericArcane generic = new GenericArcane(control0.ToString() + control1.ToString() + control2.ToString() + control3.ToString() + control4.ToString() + control5.ToString() + control6.ToString() + control7.ToString() + control8.ToString(), baseState, spellControl[0], spellControl[1], spellControl[2], spellControl[3], spellControl[4], spellControl[5], spellControl[6], spellControl[7], spellControl[8], spellControl[9], spellControl[10], spellControl[11], spellControl[12], spellControl[13], spellControl[14], spellControl[15], spellControl[16], spellControl[17], spellControl[18], spellControl[19], spellControl[20], spellControl[21], spellControl[22], spellControl[23], spellControl[24], spellControl[25], spellControl[26], spellControl[27], spellControl[28], spellControl[29], spellControl[30], spellControl[31], spellControl[32], spellControl[33], spellControl[34], spellControl[35], spellControl[36], spellControl[37], spellControl[38], spellControl[39], spellControl[40], spellControl[41], spellControl[42], spellControl[43], spellControl[44], spellControl[45]);
+                                                if (!cycleDict.ContainsKey(generic.SpellDistribution))
+                                                {
+                                                    cycleDict.Add(generic.SpellDistribution, generic);
+                                                }
+                                            }
 
             List<GenericArcane> cyclePalette = new List<GenericArcane>();
 
@@ -161,20 +185,13 @@ namespace Rawr.Mage
                 }
             } while (mindpmCycle != null);
 
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine("Optimal Cycle Palette:");
-            sb.AppendLine("");
+
             foreach (GenericArcane cycle in cyclePalette)
             {
                 sb.Append(cycle.Name + ": " + cycle.DamagePerSecond + " dps, " + cycle.ManaPerSecond + " mps\r\n");
                 //System.Diagnostics.Debug.Write(cycle.Name + ": " + cycle.DamagePerSecond + " dps, " + cycle.ManaPerSecond + " mps\r\n");
             }
-            MessageBox.Show(sb.ToString());
-
-            /*foreach (GenericArcane cycle in cycleDict.Values)
-            {
-                System.Diagnostics.Debug.Write(cycle.Name + ":\r\n" + cycle.DamagePerSecond + " dps\r\n" + cycle.ManaPerSecond + " mps\r\n" + cycle.SpellDistribution + "\r\n");
-            }*/
+            return sb;
         }
 	}
 }
