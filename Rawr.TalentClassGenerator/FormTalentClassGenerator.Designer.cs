134401c52c081b680e1e1f44f58579a2be688eb6
Kavan_cp <673bc13b9e52e766925399323aa7254450550230hcsPpcdD>
2009-04-17 12:01:54 +0000

    Talent class generator for armory.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C33239

--- Rawr.TalentClassGenerator/FormTalentClassGenerator.Designer.cs
+++ Rawr.TalentClassGenerator/FormTalentClassGenerator.Designer.cs
@@ -28,68 +28,68 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.label1 = new System.Windows.Forms.Label();
-			this.textBoxUrl = new System.Windows.Forms.TextBox();
-			this.textBoxCode = new System.Windows.Forms.TextBox();
-			this.buttonGenerateCode = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 9);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(424, 13);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "URL of the talents data.js file to parse and generate strongly typed talent class" +
-				" based on:";
-			// 
-			// textBoxUrl
-			// 
-			this.textBoxUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
-			this.textBoxUrl.Location = new System.Drawing.Point(12, 25);
-			this.textBoxUrl.Name = "textBoxUrl";
-			this.textBoxUrl.Size = new System.Drawing.Size(498, 20);
-			this.textBoxUrl.TabIndex = 0;
-			this.textBoxUrl.Text = "http://www.worldofwarcraft.com/shared/global/talents/wrath/{0}/data.js";
-			// 
-			// textBoxCode
-			// 
-			this.textBoxCode.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-						| System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
-			this.textBoxCode.Location = new System.Drawing.Point(12, 80);
-			this.textBoxCode.Multiline = true;
-			this.textBoxCode.Name = "textBoxCode";
-			this.textBoxCode.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-			this.textBoxCode.Size = new System.Drawing.Size(498, 383);
-			this.textBoxCode.TabIndex = 3;
-			// 
-			// buttonGenerateCode
-			// 
-			this.buttonGenerateCode.Location = new System.Drawing.Point(12, 51);
-			this.buttonGenerateCode.Name = "buttonGenerateCode";
-			this.buttonGenerateCode.Size = new System.Drawing.Size(87, 23);
-			this.buttonGenerateCode.TabIndex = 1;
-			this.buttonGenerateCode.Text = "Generate Code";
-			this.buttonGenerateCode.UseVisualStyleBackColor = true;
-			this.buttonGenerateCode.Click += new System.EventHandler(this.buttonGenerateCode_Click);
-			// 
-			// FormTalentClassGenerator
-			// 
-			this.AcceptButton = this.buttonGenerateCode;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(522, 475);
-			this.Controls.Add(this.buttonGenerateCode);
-			this.Controls.Add(this.textBoxCode);
-			this.Controls.Add(this.textBoxUrl);
-			this.Controls.Add(this.label1);
-			this.Name = "FormTalentClassGenerator";
-			this.Text = "Talent Class Generator";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.textBoxCode = new System.Windows.Forms.TextBox();
+            this.buttonGenerateCode = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(408, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL of the talents xml file to parse and generate strongly typed talent class bas" +
+                "ed on:";
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxUrl.Location = new System.Drawing.Point(12, 25);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(498, 20);
+            this.textBoxUrl.TabIndex = 0;
+            this.textBoxUrl.Text = "http://www.wowarmory.com/talent-tree.xml?cid={0}";
+            // 
+            // textBoxCode
+            // 
+            this.textBoxCode.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxCode.Location = new System.Drawing.Point(12, 80);
+            this.textBoxCode.Multiline = true;
+            this.textBoxCode.Name = "textBoxCode";
+            this.textBoxCode.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxCode.Size = new System.Drawing.Size(498, 383);
+            this.textBoxCode.TabIndex = 3;
+            // 
+            // buttonGenerateCode
+            // 
+            this.buttonGenerateCode.Location = new System.Drawing.Point(12, 51);
+            this.buttonGenerateCode.Name = "buttonGenerateCode";
+            this.buttonGenerateCode.Size = new System.Drawing.Size(87, 23);
+            this.buttonGenerateCode.TabIndex = 1;
+            this.buttonGenerateCode.Text = "Generate Code";
+            this.buttonGenerateCode.UseVisualStyleBackColor = true;
+            this.buttonGenerateCode.Click += new System.EventHandler(this.buttonGenerateCode_Click);
+            // 
+            // FormTalentClassGenerator
+            // 
+            this.AcceptButton = this.buttonGenerateCode;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 475);
+            this.Controls.Add(this.buttonGenerateCode);
+            this.Controls.Add(this.textBoxCode);
+            this.Controls.Add(this.textBoxUrl);
+            this.Controls.Add(this.label1);
+            this.Name = "FormTalentClassGenerator";
+            this.Text = "Talent Class Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
