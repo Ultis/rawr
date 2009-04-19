a0fba19b8ca19ed4c09ab0528a3186ecb9329778
Levva_cp <14064c3e782a7146c456bfa83e60e17ffb159ee62zdfWKTm>
2009-04-19 12:36:36 +0000

    Enhance : Fixed Export to sim including ED procced crit values
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C33318

--- Rawr.Enhance/EnhSim.cs
+++ Rawr.Enhance/EnhSim.cs
@@ -42,7 +42,7 @@ namespace Rawr.Enhance
             sb.AppendLine("oh_speed                        " + OHSpeed.ToString());
             sb.AppendLine("mh_dps                          " + wdpsMH.ToString("F1", CultureInfo.InvariantCulture));
             sb.AppendLine("oh_dps                          " + wdpsOH.ToString("F1", CultureInfo.InvariantCulture));
-            float chanceCrit = cs.ChanceWhiteCrit * 100f;
+            float chanceCrit = (cs.ChanceWhiteCrit - cs.EDBonusCrit) * 100f;
             sb.AppendLine("mh_crit                         " + chanceCrit.ToString("F2", CultureInfo.InvariantCulture));
             sb.AppendLine("oh_crit                         " + chanceCrit.ToString("F2", CultureInfo.InvariantCulture));
             float hitBonus = StatConversion.GetHitFromRating(stats.HitRating) * 100f;
