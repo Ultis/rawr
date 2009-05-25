57678ec4b16fbdb41b0818d858fb0be919bf06fe
Unknown TFS user <unknown@tfs.local>
2009-05-25 01:10:19 +0000

    File missing from previous build, PetSkills.cs now present
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C34150

---
+++ Rawr.Hunter/PetSkill.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rawr.Hunter
+{
+    public class PetSkill
+    {
+        public double CD;
+        public int Cost;
+        public int Talent;
+        public int Type;
+        public int Min;
+        public int Max;
+
+        public PetSkill(double cd, int cost, int talent, int type)
+        {
+            CD = cd;
+            Cost = cost;
+            Talent = talent;
+            Type = type;
+            Min = 0;
+            Max = 0;
+        }
+
+        public PetSkill(double cd, int cost, int talent, int type, int min, int max)
+        {
+            CD = cd;
+            Cost = cost;
+            Talent = talent;
+            Type = type;
+            Min = min;
+            Max = max;
+        }
+    }
+}
