a973cd84d405bb1d717446d147429d0718052c08
Unknown TFS user <unknown@tfs.local>
2009-06-04 18:20:50 +0000

    Rawr.Base: Updated ArmorPenetration constant to reflect the 1.25% buff that was added a while back
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C34400

--- Rawr.Base/StatConversion.cs
+++ Rawr.Base/StatConversion.cs
@@ -35,7 +35,7 @@ namespace Rawr
         #region Character Constants
 
         public const float LEVEL_80_COMBATRATING_MODIFIER = 3.2789987789987789987789987789988f; // 82/52 * Math.Pow(131/63, ((80 - 70) / 10));
-        public const float RATING_PER_ARMORPENETRATION = 1231.6239f; // 4.69512177f / 1.25f * LEVEL_80_COMBATRATING_MODIFIER * 100f;
+        public const float RATING_PER_ARMORPENETRATION = 1539.529875f; // 4.69512177f / 1.25f * LEVEL_80_COMBATRATING_MODIFIER * 100f;
         public const float RATING_PER_BLOCK = 1639.4994f; // 5f * LEVEL_80_COMBATRATING_MODIFIER * 100f;
         public const float RATING_PER_DEFENSE = 4.9184987f; // 1.5f * LEVEL_80_COMBATRATING_MODIFIER;
         public const float RATING_PER_DODGE = 3934.7985f; // 12f * LEVEL_80_COMBATRATING_MODIFIER * 100f;
