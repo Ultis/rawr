f912463b321b0be3ac00bb2ec1078a3eb0346882
Ermad_cp <f0f72a736fc87c6bddb1ad1c9da8193512a0ac44B8bybe4x>
2009-06-20 23:09:04 +0000

    Rawr3: Implemented PHP version of Armory redirect (since our hosting doesn't support ASPX). Aspx version is still used when DEBUG symbol is set, so Armory loading works when testing from localhost server.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C34804

--- Rawr3/Rawr.Silverlight.Web/ClientBin/Armory.aspx.cs
+++ Rawr3/Rawr.Silverlight.Web/ClientBin/Armory.aspx.cs
@@ -12,19 +12,27 @@ namespace Rawr.Silverlight.Web
         {
 
             string my = HttpContext.Current.Request.Url.ToString();
-            string url = string.Format("http://www.wowarmory.com/{0}", my.Substring(my.IndexOf('?')+1).Replace('*','?'));
+            string[] array = my.Substring(my.IndexOf('?') + 1).Split('*');
 
-            HttpWebRequest loHttp = (HttpWebRequest)WebRequest.Create(url);
-            loHttp.Timeout = 10000;
-            loHttp.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.8.1.4) Gecko/20070515 Firefox/2.0.0.4";
-            HttpWebResponse loWebResponse = (HttpWebResponse)loHttp.GetResponse();
-            Encoding enc = Encoding.GetEncoding(1252);  // Windows default Code Page
-            StreamReader loResponseStream = new StreamReader(loWebResponse.GetResponseStream(), enc);
-            string lcHtml = loResponseStream.ReadToEnd();
-            loWebResponse.Close();
-            loResponseStream.Close();
+            string url = "";
 
-            MainLiteral.Text = lcHtml;
+            if (array.Length == 2 || array.Length == 3)
+            {
+                if (array.Length == 2) url = string.Format("http://www.wowarmory.com/{0}?{1}", array[0], array[1]);
+                else url = string.Format("http://{0}.wowarmory.com/{1}?{2}", array[0], array[1], array[2]);
+
+                HttpWebRequest loHttp = (HttpWebRequest)WebRequest.Create(url);
+                loHttp.Timeout = 10000;
+                loHttp.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.8.1.4) Gecko/20070515 Firefox/2.0.0.4";
+                HttpWebResponse loWebResponse = (HttpWebResponse)loHttp.GetResponse();
+                Encoding enc = Encoding.GetEncoding(1252);  // Windows default Code Page
+                StreamReader loResponseStream = new StreamReader(loWebResponse.GetResponseStream(), enc);
+                string lcHtml = loResponseStream.ReadToEnd();
+                loWebResponse.Close();
+                loResponseStream.Close();
+
+                MainLiteral.Text = lcHtml;
+            }
         }
     }
 }
