3a6b338ac13e1eb0a5d93826b5e78435745c2074
Shazear_cp <10c9ac5865e763141149f2b1a65e9add4360a2659J3JrJxD>
2009-08-28 23:05:53 +0000

    Initial check in for Rawr3.TankDK - it builds.  Not sure beyond that.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C36361

---
+++ Rawr3/Rawr.TankDK/CalculationOptionsPanelTankDK.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Rawr.TankDK
+{
+    public partial class CalculationOptionsPanelTankDK : UserControl, ICalculationOptionsPanel
+    {
+        public CalculationOptionsPanelTankDK()
+        {
+            InitializeComponent();
+        }
+        public UserControl PanelControl { get { return this; } }
+
+        private Character character;
+        public Character Character
+        {
+            get { return character; }
+            set
+            {
+                character = value;
+                LoadCalculationOptions();
+            }
+        }
+
+        private bool _loadingCalculationOptions;
+        public void LoadCalculationOptions()
+        {
+            _loadingCalculationOptions = true;
+            if (Character.CalculationOptions == null) Character.CalculationOptions = new CalculationOptionsTankDK();
+
+            _loadingCalculationOptions = false;
+        }
+    }
+}
---
+++ Rawr3/Rawr.TankDK/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rawr.TankDK")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Symantec")]
+[assembly: AssemblyProduct("Rawr.TankDK")]
+[assembly: AssemblyCopyright("Copyright © Symantec 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("02a84ddc-5aab-42dd-a468-bf604c8f749a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
