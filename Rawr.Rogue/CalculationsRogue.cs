b1e34ad6c378c118fa051e691e936a10cc6616f1
Jothay_cp <89f07ab5a0412695c4dbd13d89fded8c68adf163nkKyjNSt>
2009-10-12 16:19:21 +0000

    Rawr.Rogue: Fix for issue 14403, default gemming templates to Epic instead of Rare
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C37475

--- Rawr.Rogue/CalculationsRogue.cs
+++ Rawr.Rogue/CalculationsRogue.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+#if RAWR3
+using System.Windows.Media;
+#else
+using System.Drawing;
+#endif
 using System.IO;
 using System.Xml.Serialization;
 using Rawr.Rogue.ClassAbilities;
 using Rawr.Rogue.FinishingMoves;
 using Rawr.Rogue.Poisons;
 using Rawr.Rogue.SpecialAbilities;
-#if RAWR3
-    using System.Windows.Media;
-#else
-    using System.Drawing;
-#endif
 
 namespace Rawr.Rogue {
     [Calculations.RawrModelInfoAttribute("Rogue", "Ability_Rogue_SliceDice", CharacterClass.Rogue)]
@@ -19,28 +19,28 @@ namespace Rawr.Rogue {
             get {
                 ////Relevant Gem IDs for Rogues
                 //Red
-                int[] bold = { 39900, 39996, 40111, 42142 };
-                int[] delicate = { 39905, 39997, 40112, 42143 };
+                int[] bold      = { 39900, 39996, 40111, 42142 };
+                int[] delicate  = { 39905, 39997, 40112, 42143 };
 
                 //Purple
-                int[] shifting = { 39935, 40023, 40130 };
+                int[] shifting  = { 39935, 40023, 40130 };
                 int[] sovereign = { 39934, 40022, 40129 };
 
                 //Blue
-                int[] solid = { 39919, 40008, 40119, 36767 };
+                int[] solid     = { 39919, 40008, 40119, 36767 };
 
                 //Green
-                int[] enduring = { 39976, 40089, 40167 };
+                int[] enduring  = { 39976, 40089, 40167 };
 
                 //Yellow
-                int[] thick = { 39916, 40015, 40126, 42157 };
+                int[] thick     = { 39916, 40015, 40126, 42157 };
 
                 //Orange
-                int[] etched = { 39948, 40038, 40143 };
-                int[] fierce = { 39951, 40041, 40146 };
-                int[] glinting = { 39953, 40044, 40148 };
-                int[] stalwart = { 39964, 40056, 40160 };
-                int[] deadly = { 39952, 40043, 40147 };
+                int[] etched    = { 39948, 40038, 40143 };
+                int[] fierce    = { 39951, 40041, 40146 };
+                int[] glinting  = { 39953, 40044, 40148 };
+                int[] stalwart  = { 39964, 40056, 40160 };
+                int[] deadly    = { 39952, 40043, 40147 };
 
                 //Meta
                 // int austere = 41380;
@@ -51,13 +51,13 @@ namespace Rawr.Rogue {
 					    RedId = delicate[0], YellowId = delicate[0], BlueId = delicate[0], PrismaticId = delicate[0], MetaId = relentless },
 				    new GemmingTemplate { Model = "Rogue", Group = "Uncommon", //Agi/Crit
 					    RedId = delicate[0], YellowId = deadly[0], BlueId = shifting[0], PrismaticId = delicate[0], MetaId = relentless },
-				    new GemmingTemplate { Model = "Rogue", Group = "Rare", Enabled = true, //Max Agility
+				    new GemmingTemplate { Model = "Rogue", Group = "Rare",  //Max Agility
 					    RedId = delicate[1], YellowId = delicate[1], BlueId = delicate[1], PrismaticId = delicate[1], MetaId = relentless },
-				    new GemmingTemplate { Model = "Rogue", Group = "Rare", Enabled = true, //Agi/Crit 
+				    new GemmingTemplate { Model = "Rogue", Group = "Rare",  //Agi/Crit 
 					    RedId = delicate[1], YellowId = deadly[1], BlueId = shifting[1], PrismaticId = delicate[1], MetaId = relentless },
-				    new GemmingTemplate { Model = "Rogue", Group = "Epic", //Max Agility
+				    new GemmingTemplate { Model = "Rogue", Group = "Epic", Enabled = true, //Max Agility
 					    RedId = delicate[2], YellowId = delicate[2], BlueId = delicate[2], PrismaticId = delicate[2], MetaId = relentless },
-				    new GemmingTemplate { Model = "Rogue", Group = "Epic", //Agi/Crit 
+				    new GemmingTemplate { Model = "Rogue", Group = "Epic", Enabled = true, //Agi/Crit 
 					    RedId = delicate[2], YellowId = deadly[2], BlueId = shifting[2], PrismaticId = delicate[2], MetaId = relentless },
 				    new GemmingTemplate { Model = "Rogue", Group = "Jeweler", //Max Agility
 					    RedId = delicate[3], YellowId = delicate[3], BlueId = delicate[3], PrismaticId = delicate[3], MetaId = relentless },
