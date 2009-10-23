70f630489aee07176853ee7c983d5d3259cb46ff
roncli_cp <7a5d5b9b6bc233f505ac4c35f99a2b8756fd57e8JsFKjaCf>
2009-10-23 22:56:22 +0000

    Rawr3.ProtPaladin - Target tab on the options panel is complete, still have to add ranking and abilities
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C37789

--- Rawr3/Rawr.ProtPaladin/CalculationOptionsPanelProtPaladin.xaml.cs
+++ Rawr3/Rawr.ProtPaladin/CalculationOptionsPanelProtPaladin.xaml.cs
@@ -14,15 +14,65 @@ using System.ComponentModel;
 namespace Rawr.ProtPaladin
 {
 	public partial class CalculationOptionsPanelProtPaladin : UserControl, ICalculationOptionsPanel
-	{
-		public CalculationOptionsPanelProtPaladin()
+    {
+
+        #region Initialization
+
+        private static string[] TargetTypes = new string[]
+        {
+            "Unspecified",
+            "Humanoid",
+            "Undead",
+            "Demon",
+            "Elemental",
+            "Giant",
+            "Mechanical",
+            "Beast",
+            "Dragonkin"
+        };
+
+        private static string[] MagicDamageTypes = new string[]
+        {
+            "None",
+            "Physical",
+            "Holy",
+            "Fire",
+            "Nature",
+            "Frost",
+            "Frostfire",
+            "Shadow",
+            "Arcane",
+            "Spellfire",
+            "Naturefire",
+        };
+
+        private Dictionary<int, string> armorBosses = new Dictionary<int, string>();
+
+        public CalculationOptionsPanelProtPaladin()
 		{
 			InitializeComponent();
-		}
 
-		public UserControl PanelControl { get { return this; } }
+            // Setup TargetType combo box
+            cboTargetType.ItemsSource = TargetTypes;
+
+            // Setup MagicDamageType combo box
+            cboMagicDamageType.ItemsSource = MagicDamageTypes;
+
+            // Setup TargetArmor values
+            armorBosses.Add((int)StatConversion.NPC_ARMOR[80 - 80], "Level 80 Creatures");
+            armorBosses.Add((int)StatConversion.NPC_ARMOR[81 - 80], "Level 81 Creatures");
+            armorBosses.Add((int)StatConversion.NPC_ARMOR[82 - 80], "Level 82 Creatures");
+            armorBosses.Add((int)StatConversion.NPC_ARMOR[83 - 80], "Bosses & Level 83 Creatures");
+        }
+
+        #endregion
+
+        #region ICalculationOptionsPanel
+
+        public UserControl PanelControl { get { return this; } }
 
 		private Character character;
+        private CalculationOptionsProtPaladin calcOpts;
 		public Character Character
 		{
 			get
@@ -38,11 +88,11 @@ namespace Rawr.ProtPaladin
 
                 character = value;
                 if (character.CalculationOptions == null)
+                {
                     character.CalculationOptions = new CalculationOptionsProtPaladin();
+                }
 
-                LayoutRoot.DataContext = Character.CalculationOptions;
-
-                CalculationOptionsProtPaladin calcOpts = character.CalculationOptions as CalculationOptionsProtPaladin;
+                calcOpts = character.CalculationOptions as CalculationOptionsProtPaladin;
 
                 DataContext = calcOpts;
 
@@ -54,5 +104,55 @@ namespace Rawr.ProtPaladin
         {
             Character.OnCalculationsInvalidated();
         }
-	}
+
+        #endregion
+
+        #region Events
+
+        private void sliTargetArmor_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (tbTargetArmor != null)
+                tbTargetArmor.Text = e.NewValue.ToString() + (armorBosses.ContainsKey((int)e.NewValue) ? ": " + armorBosses[(int)e.NewValue] : "");
+        }
+
+        private void btnResetTargetArmor_Click(object sender, RoutedEventArgs e)
+        {
+            calcOpts.TargetArmor = (int)StatConversion.NPC_ARMOR[calcOpts.TargetLevel - 80];
+        }
+
+        private void btnResetBossAttackValue_Click(object sender, RoutedEventArgs e)
+        {
+            calcOpts.BossAttackValue = 80000;
+        }
+
+        private void sliBossAttackSpeed_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (tbBossAttackSpeed != null)
+                tbBossAttackSpeed.Text = string.Format("{0:N2} seconds", e.NewValue);
+        }
+
+        private void btnResetBossAttackSpeed_Click(object sender, RoutedEventArgs e)
+        {
+            calcOpts.BossAttackSpeed = 1.0f;
+        }
+
+        private void btnResetBossAttackValueMagic_Click(object sender, RoutedEventArgs e)
+        {
+            calcOpts.BossAttackValueMagic = 20000;
+        }
+
+        private void silBossAttackSpeedMagic_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (tbBossAttackSpeedMagic != null)
+                tbBossAttackSpeedMagic.Text = string.Format("{0:N2} seconds", e.NewValue);
+        }
+
+        private void btnResetBossAttackSpeedMagic_Click(object sender, RoutedEventArgs e)
+        {
+            calcOpts.BossAttackSpeedMagic = 2.0f;
+        }
+
+        #endregion
+
+    }
 }
