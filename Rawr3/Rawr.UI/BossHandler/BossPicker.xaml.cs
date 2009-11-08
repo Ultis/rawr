b71f47cbc825f9fa842ba1ebf5787c3e4ba5e6fb
Jothay_cp <89f07ab5a0412695c4dbd13d89fded8c68adf163nkKyjNSt>
2009-11-08 22:00:06 +0000

    Rawr3.BossHandler: Adding BossHandler tab to project
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C39075

---
+++ Rawr3/Rawr.UI/BossHandler/BossPicker.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Rawr.UI
+{
+    public partial class BossPicker : UserControl
+    {
+        public BossPicker()
+        {
+            InitializeComponent();
+        }
+        private Character character;
+        public Character Character
+        {
+            get { return character; }
+            set {
+                if (character != null) character.ClassChanged -= new EventHandler(character_ClassChanged);
+                character = value;
+                if (character != null)
+                {
+                    character.ClassChanged += new EventHandler(character_ClassChanged);
+                    character_ClassChanged(this, EventArgs.Empty);
+                }
+            }
+        }
+        private void character_ClassChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
