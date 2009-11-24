3d7f6898917d0786b5d08714b12365e2d9515826
Levva_cp <14064c3e782a7146c456bfa83e60e17ffb159ee62zdfWKTm>
2009-11-24 23:13:33 +0000

    Enhance : Change Hide Professions to use global setting
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C42109

--- Rawr.Enhance/CalculationsEnhance.cs
+++ Rawr.Enhance/CalculationsEnhance.cs
@@ -586,10 +586,9 @@ namespace Rawr.Enhance
         #region Get Character Stats
         public override Stats GetCharacterStats(Character character, Item additionalItem)
 		{
-            CalculationOptionsEnhance calcOpts = character.CalculationOptions as CalculationOptionsEnhance;
             Stats statsBase = BaseStats.GetBaseStats(character); 
             Stats statsBaseGear = GetItemStats(character, additionalItem);
-			Stats statsBuffs = GetBuffsStats(character, calcOpts);
+			Stats statsBuffs = GetBuffsStats(character.ActiveBuffs);
             Stats statsGearEnchantsBuffs = statsBaseGear + statsBuffs;
 
 			int AK = character.ShamanTalents.AncestralKnowledge;
@@ -658,98 +657,6 @@ namespace Rawr.Enhance
         #endregion
 
         #region Buff Functions
-        public Stats GetBuffsStats(Character character, CalculationOptionsEnhance calcOpts) {
-            List<Buff> removedBuffs = new List<Buff>();
-            List<Buff> addedBuffs = new List<Buff>();
-
-            //float hasRelevantBuff;
-
-            #region Racials to Force Enable
-            // Draenei should always have this buff activated
-            // NOTE: for other races we don't wanna take it off if the user has it active, so not adding code for that
-            if (character.Race == CharacterRace.Draenei
-                && !character.ActiveBuffs.Contains(Buff.GetBuffByName("Heroic Presence")))
-            {
-                character.ActiveBuffsAdd(("Heroic Presence"));
-            }
-            #endregion
-
-            #region Passive Ability Auto-Fixing
-            // Removes the Trueshot Aura Buff and it's equivalents Unleashed Rage and Abomination's Might if you are
-            // maintaining it yourself. We are now calculating this internally for better accuracy and to provide
-            // value to relevant talents
-            /*{
-                hasRelevantBuff = character.HunterTalents.TrueshotAura;
-                Buff a = Buff.GetBuffByName("Trueshot Aura");
-                Buff b = Buff.GetBuffByName("Unleashed Rage");
-                Buff c = Buff.GetBuffByName("Abomination's Might");
-                if (hasRelevantBuff > 0)
-                {
-                    if (character.ActiveBuffs.Contains(a)) { character.ActiveBuffs.Remove(a); removedBuffs.Add(a); }
-                    if (character.ActiveBuffs.Contains(b)) { character.ActiveBuffs.Remove(b); removedBuffs.Add(b); }
-                    if (character.ActiveBuffs.Contains(c)) { character.ActiveBuffs.Remove(c); removedBuffs.Add(c); }
-                }
-            }
-            // Removes the Hunter's Mark Buff and it's Children 'Glyphed', 'Improved' and 'Both' if you are
-            // maintaining it yourself. We are now calculating this internally for better accuracy and to provide
-            // value to relevant talents
-            {
-                hasRelevantBuff =  character.HunterTalents.ImprovedHuntersMark
-                                + (character.HunterTalents.GlyphOfHuntersMark ? 1 : 0);
-                Buff a = Buff.GetBuffByName("Hunter's Mark");
-                Buff b = Buff.GetBuffByName("Glyphed Hunter's Mark");
-                Buff c = Buff.GetBuffByName("Improved Hunter's Mark");
-                Buff d = Buff.GetBuffByName("Improved and Glyphed Hunter's Mark");
-                // Since we are doing base Hunter's mark ourselves, we still don't want to double-dip
-                if (character.ActiveBuffs.Contains(a)) { character.ActiveBuffs.Remove(a); /*removedBuffs.Add(a);*//* }
-                // If we have an enhanced Hunter's Mark, kill the Buff
-                if (hasRelevantBuff > 0) {
-                    if (character.ActiveBuffs.Contains(b)) { character.ActiveBuffs.Remove(b); /*removedBuffs.Add(b);*//* }
-                    if (character.ActiveBuffs.Contains(c)) { character.ActiveBuffs.Remove(c); /*removedBuffs.Add(c);*//* }
-                    if (character.ActiveBuffs.Contains(d)) { character.ActiveBuffs.Remove(d); /*removedBuffs.Add(c);*//* }
-                }
-            }*/
-            /* [More Buffs to Come to this method]
-             * Ferocious Inspiration | Sanctified Retribution
-             * Hunting Party | Judgements of the Wise, Vampiric Touch, Improved Soul Leech, Enduring Winter
-             * Acid Spit | Expose Armor, Sunder Armor (requires BM & Worm Pet)
-             */
-            #endregion
-
-            #region Special Pot Handling
-            /*foreach (Buff potionBuff in character.ActiveBuffs.FindAll(b => b.Name.Contains("Potion")))
-            {
-                if (potionBuff.Stats._rawSpecialEffectData != null
-                    && potionBuff.Stats._rawSpecialEffectData[0] != null)
-                {
-                    Stats newStats = new Stats();
-                    newStats.AddSpecialEffect(new SpecialEffect(potionBuff.Stats._rawSpecialEffectData[0].Trigger,
-                                                                potionBuff.Stats._rawSpecialEffectData[0].Stats,
-                                                                potionBuff.Stats._rawSpecialEffectData[0].Duration,
-                                                                calcOpts.Duration,
-                                                                potionBuff.Stats._rawSpecialEffectData[0].Chance,
-                                                                potionBuff.Stats._rawSpecialEffectData[0].MaxStack));
-
-                    Buff newBuff = new Buff() { Stats = newStats };
-                    character.ActiveBuffs.Remove(potionBuff);
-                    character.ActiveBuffsAdd(newBuff);
-                    removedBuffs.Add(potionBuff);
-                    addedBuffs.Add(newBuff);
-                }
-            }*/
-            #endregion
-
-            Stats statsBuffs = GetBuffsStats(character.ActiveBuffs);
-
-            foreach (Buff b in removedBuffs) {
-                character.ActiveBuffsAdd(b);
-            }
-            foreach (Buff b in addedBuffs) {
-                character.ActiveBuffs.Remove(b);
-            }
-
-            return statsBuffs;
-        }
         public override void SetDefaults(Character character)
         {
             // add shaman buffs
@@ -888,12 +795,56 @@ namespace Rawr.Enhance
 			return base.IsItemRelevant(item);
 		}
 
+        private static Character cacheChar = null;
+
+        public bool CheckHasProf(Profession p)
+        {
+            if (cacheChar == null) 
+                return false;
+            return cacheChar.PrimaryProfession == p || cacheChar.SecondaryProfession == p;
+        }
+
         public override bool IsEnchantRelevant(Enchant enchant)
         {
             string name = enchant.Name;
             if (name.Contains("Rune of the Fallen Crusader"))
                 return false; // Bad DK Enchant, Bad!
-            return IsProfEnchantRelevant(enchant) && (HasRelevantStats(enchant.Stats));
+            else if (Rawr.Properties.GeneralSettings.Default.HideProfEnchants)
+            {
+                if (!CheckHasProf(Profession.Enchanting))
+                {
+                    if (enchant.Slot == ItemSlot.Finger)
+                        return false;
+                }
+                if (!CheckHasProf(Profession.Engineering))
+                {
+                    if (name.Contains("Mind Amplification Dish") ||
+                        name.Contains("Flexweave Underlay") ||
+                        name.Contains("Hyperspeed Accelerators") ||
+                        name.Contains("Reticulated Armor Webbing") ||
+                        name.Contains("Nitro Boosts")  ||
+                        name.Contains("Springy Arachnoweave"))
+                    {
+                        return false;
+                    }
+                }
+                if (!CheckHasProf(Profession.Inscription))
+                {
+                    if (name.Contains("Master's") || name.Contains("Inscription of Triumph"))
+                        return false;
+                }
+                if (!CheckHasProf(Profession.Leatherworking))
+                {
+                    if (name.Contains("Fur Lining") || name.Contains("Jormungar") || name.Contains("Magister's Armor Kit") || name.Contains("Nerubian Leg Reinforcements"))
+                        return false;
+                }
+                if (!CheckHasProf(Profession.Tailoring))
+                {
+                    if (name.Contains("Embroidery"))
+                        return false;
+                }
+            }
+            return HasRelevantStats(enchant.Stats);
         }
 
 		public override Stats GetRelevantStats(Stats stats)
