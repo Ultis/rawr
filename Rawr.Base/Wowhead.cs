430bbccd22b535788e35b85f5cc106451d35f762
Kavan_cp <673bc13b9e52e766925399323aa7254450550230hcsPpcdD>
2010-02-15 23:34:53 +0000

    Base: Fix for wowhead parsing of Wrathful set names.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C46511

--- Rawr.Base/Wowhead.cs
+++ Rawr.Base/Wowhead.cs
@@ -939,7 +939,7 @@ namespace Rawr
             {
                 htmlTooltip = htmlTooltip.Substring(0, htmlTooltip.IndexOf("</a> (0/"));
                 htmlTooltip = htmlTooltip.Substring(htmlTooltip.LastIndexOf(">") + 1);
-                htmlTooltip = htmlTooltip.Replace("Relentless ", "").Replace("Furious ", "").Replace("Deadly ", "").Replace("Hateful ", "").Replace("Savage ", "")
+                htmlTooltip = htmlTooltip.Replace("Wrathful ", "").Replace("Relentless ", "").Replace("Furious ", "").Replace("Deadly ", "").Replace("Hateful ", "").Replace("Savage ", "")
                     .Replace("Brutal ", "").Replace("Vengeful ", "").Replace("Merciless ", "").Replace("Valorous ", "")
                     .Replace("Heroes' ", "").Replace("Conqueror's ", "").Replace("Totally ", "").Replace("Triumphant ", "").Replace("Kirin'dor", "Kirin Tor").Replace("Regaila", "Regalia").Replace("Sanctified ", "");
 
