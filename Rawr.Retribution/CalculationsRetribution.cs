2bd3c5c2b53210803bf10571d30d1cea9dd98352
OReubens_cp <6573efd21990e0da7ca627e897d4ec9ab40e9a68CEZX46pN>
2010-03-05 19:56:06 +0000

    Rawr.Retribution: reverting previous change
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C47149

--- Rawr.Retribution/CalculationsRetribution.cs
+++ Rawr.Retribution/CalculationsRetribution.cs
@@ -614,13 +614,11 @@ namespace Rawr.Retribution
             switch (effect.Trigger)
             {
                 case Trigger.MeleeCrit:
-                    //trigger = 1f / rot.GetMeleeCritsPerSec();
-                    trigger = 1f / rot.GetPhysicalCritsPerSec();  // For Ret, meleeHit and PhysicalHit are the same. Judgements and HoW also trigger MeleeHit effects.
+                    trigger = 1f / rot.GetMeleeCritsPerSec();
                     break;
 
                 case Trigger.MeleeHit:
-                    //trigger = 1f / rot.GetMeleeAttacksPerSec();
-                    trigger = 1f / rot.GetPhysicalAttacksPerSec();  // For Ret, meleeHit and PhysicalHit are the same. Judgements and HoW also trigger MeleeHit effects.
+                    trigger = 1f / rot.GetMeleeAttacksPerSec();
                     break;
 
                 case Trigger.PhysicalCrit:
