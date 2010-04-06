082ddb120abcdba831b47075ad4b50006c02589d
Shazear_cp <10c9ac5865e763141149f2b1a65e9add4360a2659J3JrJxD>
2010-04-06 18:23:27 +0000

    TankDK: No Defect: Update Threat values table from in-game research and matching tankspot post. No Defect: Off-hands were providing value in certain cases when 2h-ers were equiped.  Finally rooted out the problem.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C49175

--- Rawr.TankDK/DKAbilities/CombatTable2.cs
+++ Rawr.TankDK/DKAbilities/CombatTable2.cs
@@ -69,29 +69,34 @@ namespace Rawr.TankDK
         /// </summary>
         public void CompileRotation(Rotation Rot)
         {
+            int i = 0;
             // Build the rotation into a list of abilities to play with.
             ml_Rot = new List<AbilityDK_Base>();
             #region Runes
             #region Frost
             if (Rot.IcyTouch > 0)
             {
-                ml_Rot.Add(new AbilityDK_IcyTouch(m_Stats));
+                for (i = (int)Rot.IcyTouch; i > 0; i--)
+                    ml_Rot.Add(new AbilityDK_IcyTouch(m_Stats));
             }
             #endregion
             #region Unholy
             if (Rot.PlagueStrike > 0)
             {
-                ml_Rot.Add(new AbilityDK_PlagueStrike(m_Stats, MH, OH));
+                for (i = (int)Rot.PlagueStrike; i > 0; i--)
+                    ml_Rot.Add(new AbilityDK_PlagueStrike(m_Stats, MH, OH));
             }
             #endregion
             #region Blood
             if (Rot.BloodStrike > 0)
             {
-                ml_Rot.Add(new AbilityDK_BloodStrike(m_Stats, MH, OH));
+                for (i = (int)Rot.BloodStrike; i > 0; i--)
+                    ml_Rot.Add(new AbilityDK_BloodStrike(m_Stats, MH, OH));
             }
             if (Rot.HeartStrike > 0)
             {
-                ml_Rot.Add(new AbilityDK_HeartStrike(m_Stats, MH, OH));
+                for (i = (int)Rot.HeartStrike; i > 0; i--)
+                    ml_Rot.Add(new AbilityDK_HeartStrike(m_Stats, MH, OH));
             }
             if (Rot.BloodBoil > 0)
             {
@@ -105,15 +110,18 @@ namespace Rawr.TankDK
             #region Frost+Unholy
             if (Rot.DeathStrike > 0)
             {
-                ml_Rot.Add(new AbilityDK_DeathStrike(m_Stats, MH, OH));
+                for (i = (int)Rot.DeathStrike; i > 0; i--)
+                    ml_Rot.Add(new AbilityDK_DeathStrike(m_Stats, MH, OH));
             }
             if (Rot.Obliterate > 0)
             {
-                ml_Rot.Add(new AbilityDK_Obliterate(m_Stats, MH, OH));
+                for (i = (int)Rot.Obliterate; i > 0; i--)
+                    ml_Rot.Add(new AbilityDK_Obliterate(m_Stats, MH, OH));
             }
             if (Rot.HowlingBlast > 0)
             {
-                ml_Rot.Add(new AbilityDK_HowlingBlast(m_Stats));
+                for (i = (int)Rot.HowlingBlast; i > 0; i--)
+                    ml_Rot.Add(new AbilityDK_HowlingBlast(m_Stats));
             }
             #endregion
             #region BloodFrostUnholy
@@ -131,7 +139,8 @@ namespace Rawr.TankDK
             }
             if (Rot.DeathCoil > 0)
             {
-                ml_Rot.Add(new AbilityDK_DeathCoil(m_Stats));
+                for(i = (int)Rot.DeathCoil; i == 0; i--)
+                    ml_Rot.Add(new AbilityDK_DeathCoil(m_Stats));
             }
             if (Rot.FrostStrike > 0)
             {
@@ -140,7 +149,40 @@ namespace Rawr.TankDK
             #endregion
 
             // Now that the list is built, setup the costs so that we know what the rotation looks like in runes.
+            #region Sum of Costs
+            int[] AbilityCost = new int[(int)DKCostTypes.NumCostTypes];
+            foreach (AbilityDK_Base ability in ml_Rot)
+            {
+                for (int cost = 0; cost < (int)DKCostTypes.NumCostTypes; cost++ )
+                {
+                    AbilityCost[cost] += ability.AbilityCost[cost];
+                }
+            }
+            #endregion
 
+            // Now we have the total costs
+            // Let's evaluate the rune cost to the GCD cost:
+            // 2 runes every 10 secs (11.5 secs with a 2 sec forgiveness)
+            // in ms
+            int BloodCD = AbilityCost[(int)DKCostTypes.Blood] * 10 * 1000 / 2;
+            int FrostCD = AbilityCost[(int)DKCostTypes.Frost] * 10 * 1000 / 2;
+            int UnHolyCD = AbilityCost[(int)DKCostTypes.UnHoly] * 10 * 1000 / 2;
+
+            int maxCD = Math.Max(BloodCD, FrostCD);
+            maxCD = Math.Max(maxCD, UnHolyCD);
+            if (maxCD > AbilityCost[(int)DKCostTypes.DurationTime])
+            {
+                // We have spare GCDs waiting for Runes to come back.
+            }
+
+            // Are we starving the RSs?
+            if (AbilityCost[(int)DKCostTypes.RunicPower] > 0)
+            {
+                // starving our RP based abilities in the rotation.
+            }
+
+            // I need a way of setting up the rotation in such a way that I can get a state of the target at the time
+            // the ability is used.  
         }
 
 
