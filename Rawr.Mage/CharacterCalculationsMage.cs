56c8eefcacb001b209bd3d4380f255d763ce4ed7
Kavan_cp <673bc13b9e52e766925399323aa7254450550230hcsPpcdD>
2010-05-04 21:45:43 +0000

    Mage: Fix for displayed damage range when hit rate < 100%.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C50162

--- Rawr.Mage/CharacterCalculationsMage.cs
+++ Rawr.Mage/CharacterCalculationsMage.cs
@@ -438,7 +438,7 @@ namespace Rawr.Mage
                 bs = BaseState.GetSpell(spell);
                 if (bs != null)
                 {
-                    dictValues.Add(bs.Name, string.Format(spellFormatString, ((Cycle)bs).DamagePerSecond, ((Cycle)bs).ManaPerSecond, bs.ThreatPerSecond, bs.CastTime - bs.Latency, bs.SpellModifier, bs.CritRate * 100, bs.HitRate * 100, bs.CritBonus, bs.MinHitDamage / bs.HitProcs, bs.MaxHitDamage / bs.HitProcs, bs.MinCritDamage / bs.HitProcs, bs.MaxCritDamage / bs.HitProcs, ((bs.DotDamage > 0) ? ("\n" + bs.DotDamage.ToString("F") + " Dot") : ""), bs.Cost));
+                    dictValues.Add(bs.Name, string.Format(spellFormatString, ((Cycle)bs).DamagePerSecond, ((Cycle)bs).ManaPerSecond, bs.ThreatPerSecond, bs.CastTime - bs.Latency, bs.SpellModifier, bs.CritRate * 100, bs.HitRate * 100, bs.CritBonus, bs.MinHitDamage, bs.MaxHitDamage, bs.MinCritDamage, bs.MaxCritDamage, ((bs.DotDamage > 0) ? ("\n" + bs.DotDamage.ToString("F") + " Dot") : ""), bs.Cost));
                 }
             }
             if (Wand != null)
