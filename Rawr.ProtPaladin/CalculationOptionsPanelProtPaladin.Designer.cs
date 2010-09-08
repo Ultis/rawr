0f674bd31b27c04b17ad8f1f6d90486e56758c13
roncli_cp <7a5d5b9b6bc233f505ac4c35f99a2b8756fd57e8JsFKjaCf>
2010-09-08 15:34:01 +0000

    Rawr.ProtPaladin - Increased base attack maximum to 300,000 in Rawr2
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C53759

--- Rawr.ProtPaladin/CalculationOptionsPanelProtPaladin.Designer.cs
+++ Rawr.ProtPaladin/CalculationOptionsPanelProtPaladin.Designer.cs
@@ -199,7 +199,7 @@ namespace Rawr.ProtPaladin
             this.trackBarBossAttackValue.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.trackBarBossAttackValue.LargeChange = 5000;
             this.trackBarBossAttackValue.Location = new System.Drawing.Point(89, 17);
-            this.trackBarBossAttackValue.Maximum = 100000;
+            this.trackBarBossAttackValue.Maximum = 300000;
             this.trackBarBossAttackValue.Minimum = 500;
             this.trackBarBossAttackValue.Name = "trackBarBossAttackValue";
             this.trackBarBossAttackValue.Size = new System.Drawing.Size(178, 42);
