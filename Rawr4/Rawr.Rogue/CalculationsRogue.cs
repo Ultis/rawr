72a82bde5af59a3e401e6fd08d92664835c2b8fe
Fes_cp <8c20479ab00f48491257563614cd12e82da16ae0c2q2PuyT>
2010-09-20 22:30:58 +0000

    Raw4.Rogue: Removed the dagger requirement from Mutilate.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C54016

--- Rawr4/Rawr.Rogue/CalculationsRogue.cs
+++ Rawr4/Rawr.Rogue/CalculationsRogue.cs
@@ -623,8 +623,8 @@ namespace Rawr.Rogue
             float sStrikeDamageRaw = (baseDamageNorm * 1f + 180f) * (1f + stats.BonusPhysicalDamageMultiplier) * (1f + stats.BonusDamageMultiplier) * (1f + sStrikeDamageMultiplier) * mainHandModArmor;
             float mutiDamageMainRaw = (baseDamageNorm * 1f + 181f) * (1f + stats.BonusPhysicalDamageMultiplier) * (1f + stats.BonusDamageMultiplier) * (1f + (targetPoisonable ? 0.2f : 0f)) * mainHandModArmor;
             float mutiDamageOffRaw = (baseOffDamageNorm * 1f + 181f) * (1f + stats.BonusPhysicalDamageMultiplier) * (1f + stats.BonusDamageMultiplier) * (1f + (targetPoisonable ? 0.2f : 0f)) * offHandModArmor;
-            mutiDamageMainRaw *= (spec == 0 && mainHand._type == ItemType.Dagger && offHand._type == ItemType.Dagger ? 1f : 0f);
-            mutiDamageOffRaw *= (spec == 0 && mainHand._type == ItemType.Dagger && offHand._type == ItemType.Dagger ? 1f : 0f);
+            mutiDamageMainRaw *= spec == 0 ? 1f : 0f;
+            mutiDamageOffRaw *= spec == 0 ? 1f : 0f;
             float mutiDamageRaw = mutiDamageMainRaw + mutiDamageOffRaw;
             float ruptDamageRaw = (127f + 18f * 5f + 0.06f * stats.AttackPower * 5f / (3f + 5f)) * (3f + 5f + bonusRuptDuration / 2f) * (1f + stats.BonusPhysicalDamageMultiplier) * (1f + stats.BonusDamageMultiplier) * (1f + stats.BonusBleedDamageMultiplier) +
                  ChanceOnGarrRuptTickBonusDamage * 403f * (1f + stats.BonusNatureDamageMultiplier) * (1f + stats.BonusDamageMultiplier);
