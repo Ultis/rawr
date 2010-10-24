01d76e3373466383cb1d13a5af5fef4d9a5f397d
NeRane_cp <78755dab4313218cb17052a11f0bf5100e61d5d5gzrsCyDj>
2010-10-24 14:32:54 +0000

    Adding shell of AltoholicImporter to the project.  This is in-complete and should not be referenced by other modles at this time.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C54728

---
+++ Rawr4/Rawr.Base/AltoholicImporter.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+
+namespace Rawr
+{
+    public class AltoholicImporter
+    {
+
+        public AltoholicSavedVariables SavedVariables;
+
+        public AltoholicImporter(AltoholicSavedVariables Variables)
+        {
+            SavedVariables = Variables;
+        }
+
+        public AltoholicImporter(SavedVariablesDictionary SVCharacters,
+                                 SavedVariablesDictionary SVSkills,
+                                 SavedVariablesDictionary SVTalents,
+                                 SavedVariablesDictionary SVInventory,
+                                 SavedVariablesDictionary SVStats)
+        {
+            SavedVariables = new AltoholicSavedVariables(SVCharacters,
+                                                         SVSkills,
+                                                         SVTalents,
+                                                         SVInventory,
+                                                         SVStats);
+        }
+
+        public String[] GetCaracterList()
+        {
+            if (SavedVariables != null)
+            {
+                SavedVariables.Characters           
+            }
+        }
+        
+
+        /*
+         * SavedVariable Container for Easy Use
+         */ 
+        public class AltoholicSavedVariables
+        {
+            public SavedVariablesDictionary Characters;
+            public SavedVariablesDictionary Skills;
+            public SavedVariablesDictionary Talents;
+            public SavedVariablesDictionary Inventory;
+            public SavedVariablesDictionary Stats;
+
+            public  AltoholicSavedVariables(SavedVariablesDictionary SVCharacters,
+                                            SavedVariablesDictionary SVSkills,
+                                            SavedVariablesDictionary SVTalents,
+                                            SavedVariablesDictionary SVInventory,
+                                            SavedVariablesDictionary SVStats)
+            {
+                
+                Characters = SVCharacters;
+                Skills = SVSkills;
+                Talents = SVTalents;
+                Inventory = SVInventory;
+                Stats = SVStats;
+            }
+            
+        }
+
+    }
+
+}
