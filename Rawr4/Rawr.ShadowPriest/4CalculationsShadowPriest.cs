14bc9c5eacc55bcd242d5550c100a2f3c11c9fdb
Shep1987_cp <5c57758b125da16e44e432cb147460dcc866c155nSRUCwpu>
2010-11-06 22:44:35 +0000

    Rawr4.ShadowPriest -Added Relevent Stats to Speed up load time of model
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C55072

--- Rawr4/Rawr.ShadowPriest/4CalculationsShadowPriest.cs
+++ Rawr4/Rawr.ShadowPriest/4CalculationsShadowPriest.cs
@@ -39,7 +39,16 @@ namespace Rawr.ShadowPriest
              return _defaultGemmingTemplates;
             }
         }
-    
+
+        //0
+        public override ICalculationOptionBase DeserializeDataObject(string xml)
+        {
+            System.Xml.Serialization.XmlSerializer serializer =
+                new System.Xml.Serialization.XmlSerializer(typeof(CalculationOptionsShadowPriest));
+            System.IO.StringReader reader = new System.IO.StringReader(xml);
+            CalculationOptionsShadowPriest calcOpts = serializer.Deserialize(reader) as CalculationOptionsShadowPriest;
+            return calcOpts;
+        }
         //1
         private Dictionary<string, Color> _subPointNameColors = null;
         public override Dictionary<string, Color> SubPointNameColors
@@ -149,8 +158,7 @@ namespace Rawr.ShadowPriest
         public override ComparisonCalculationBase[] GetCustomChartData(Character character, string chartName)
         {
             return new ComparisonCalculationBase[0];
-        }
-        
+        }  
         //8
         private List<ItemType> _relevantItemTypes = null;
         public override List<ItemType> RelevantItemTypes
@@ -171,32 +179,158 @@ namespace Rawr.ShadowPriest
                 return _relevantItemTypes;
             }
         }
-     
         //9
         public override ComparisonCalculationBase CreateNewComparisonCalculation() { return new ComparisonCalculationShadowPriest(); }
         //10
         public override CharacterCalculationsBase CreateNewCharacterCalculations() { return new CharacterCalculationsShadowPriest(); }
         #endregion
-
-        public override CharacterClass TargetClass
-        {
-            get { throw new NotImplementedException(); }
-        }
+        //11
+        public override CharacterClass TargetClass { get { return CharacterClass.Priest; } }
+        //12-CataPass Needed
         public override Stats GetRelevantStats(Stats stats)
         {
-            throw new NotImplementedException();
+            Stats s = new Stats()
+            {
+                Stamina = stats.Stamina,
+                Health = stats.Health,
+                Resilience = stats.Resilience,
+                Intellect = stats.Intellect,
+                Mana = stats.Mana,
+                Spirit = stats.Spirit,
+                Mp5 = stats.Mp5,
+                SpellPower = stats.SpellPower,
+                SpellShadowDamageRating = stats.SpellShadowDamageRating,
+                CritRating = stats.CritRating,
+                SpellCrit = stats.SpellCrit,
+                SpellCritOnTarget = stats.SpellCritOnTarget,
+                HitRating = stats.HitRating,
+                SpellHit = stats.SpellHit,
+                SpellHaste = stats.SpellHaste,
+                HasteRating = stats.HasteRating,
+                BonusSpiritMultiplier = stats.BonusSpiritMultiplier,
+                BonusIntellectMultiplier = stats.BonusIntellectMultiplier,
+                BonusManaPotion = stats.BonusManaPotion,
+                ThreatReductionMultiplier = stats.ThreatReductionMultiplier,
+                BonusDamageMultiplier = stats.BonusDamageMultiplier,
+                BonusShadowDamageMultiplier = stats.BonusShadowDamageMultiplier,
+                BonusHolyDamageMultiplier = stats.BonusHolyDamageMultiplier,
+                BonusDiseaseDamageMultiplier = stats.BonusDiseaseDamageMultiplier,
+                PriestInnerFire = stats.PriestInnerFire,
+                MovementSpeed = stats.MovementSpeed,
+                SWPDurationIncrease = stats.SWPDurationIncrease,
+                BonusMindBlastMultiplier = stats.BonusMindBlastMultiplier,
+                MindBlastCostReduction = stats.MindBlastCostReduction,
+                ShadowWordDeathCritIncrease = stats.ShadowWordDeathCritIncrease,
+                WeakenedSoulDurationDecrease = stats.WeakenedSoulDurationDecrease,
+                DevouringPlagueBonusDamage = stats.DevouringPlagueBonusDamage,
+                MindBlastHasteProc = stats.MindBlastHasteProc,
+                PriestDPS_T9_2pc = stats.PriestDPS_T9_2pc,
+                PriestDPS_T9_4pc = stats.PriestDPS_T9_4pc,
+                PriestDPS_T10_2pc = stats.PriestDPS_T10_2pc,
+                PriestDPS_T10_4pc = stats.PriestDPS_T10_4pc,
+                BonusSpellCritMultiplier = stats.BonusSpellCritMultiplier,
+                ManaRestore = stats.ManaRestore,
+                SpellsManaReduction = stats.SpellsManaReduction,
+                HighestStat = stats.HighestStat,
+                ArcaneDamage = stats.ArcaneDamage,
+                FireDamage = stats.FireDamage,
+                FrostDamage = stats.FrostDamage,
+                HolyDamage = stats.HolyDamage,
+                NatureDamage = stats.NatureDamage,
+                ShadowDamage = stats.ShadowDamage,
+                ValkyrDamage = stats.ValkyrDamage,
+
+                /*ManaRestoreFromBaseManaPerHit = stats.ManaRestoreFromBaseManaPerHit,
+                SpellPowerFor15SecOnUse90Sec = stats.SpellPowerFor15SecOnUse90Sec,
+                SpellPowerFor15SecOnUse2Min = stats.SpellPowerFor15SecOnUse2Min,
+                SpellPowerFor20SecOnUse2Min = stats.SpellPowerFor20SecOnUse2Min,
+                HasteRatingFor20SecOnUse2Min = stats.HasteRatingFor20SecOnUse2Min,
+                HasteRatingFor20SecOnUse5Min = stats.HasteRatingFor20SecOnUse5Min,
+                SpellPowerFor10SecOnCast_15_45 = stats.SpellPowerFor10SecOnCast_15_45,
+                SpellPowerFor10SecOnHit_10_45 = stats.SpellPowerFor10SecOnHit_10_45,
+                SpellHasteFor10SecOnCast_10_45 = stats.SpellHasteFor10SecOnCast_10_45,
+                TimbalsProc = stats.TimbalsProc,
+                PendulumOfTelluricCurrentsProc = stats.PendulumOfTelluricCurrentsProc,
+                ExtractOfNecromanticPowerProc = stats.ExtractOfNecromanticPowerProc,
+                */
+
+                Armor = stats.Armor,
+                BonusArmor = stats.BonusArmor,
+                Agility = stats.Agility,
+                ArcaneResistance = stats.ArcaneResistance,
+                ArcaneResistanceBuff = stats.ArcaneResistanceBuff,
+                FireResistance = stats.FireResistance,
+                FireResistanceBuff = stats.FireResistanceBuff,
+                FrostResistance = stats.FrostResistance,
+                FrostResistanceBuff = stats.FrostResistanceBuff,
+                NatureResistance = stats.NatureResistance,
+                NatureResistanceBuff = stats.NatureResistanceBuff,
+                ShadowResistance = stats.ShadowResistance,
+                ShadowResistanceBuff = stats.ShadowResistanceBuff,
+            };
+
+            return s;
         }
+        //13-CataPass Needed
         public override bool HasRelevantStats(Stats stats)
         {
-            throw new NotImplementedException();
-        }
-        public override ICalculationOptionBase DeserializeDataObject(string xml)
-        {
-            System.Xml.Serialization.XmlSerializer serializer =
-                new System.Xml.Serialization.XmlSerializer(typeof(CalculationOptionsShadowPriest));
-            System.IO.StringReader reader = new System.IO.StringReader(xml);
-            CalculationOptionsShadowPriest calcOpts = serializer.Deserialize(reader) as CalculationOptionsShadowPriest;
-            return calcOpts;
+            #region Yes
+            bool Yes = (
+                stats.Intellect + stats.Mana + stats.Spirit + stats.Mp5 + stats.SpellPower
+                + stats.SpellShadowDamageRating + stats.CritRating
+                + stats.SpellCrit + stats.HitRating + stats.SpellHit + stats.SpellCritOnTarget
+                + stats.SpellHaste + stats.HasteRating
+
+                + stats.BonusSpiritMultiplier
+                + stats.BonusIntellectMultiplier + stats.BonusManaPotion
+                + stats.ThreatReductionMultiplier + stats.BonusDamageMultiplier
+                + stats.BonusShadowDamageMultiplier + stats.BonusHolyDamageMultiplier
+                + stats.BonusDiseaseDamageMultiplier
+                + stats.PriestInnerFire + stats.MovementSpeed
+
+                + stats.SWPDurationIncrease + stats.BonusMindBlastMultiplier
+                + stats.MindBlastCostReduction + stats.ShadowWordDeathCritIncrease
+                + stats.WeakenedSoulDurationDecrease
+                + stats.DevouringPlagueBonusDamage + stats.MindBlastHasteProc
+                + stats.PriestDPS_T9_2pc + stats.PriestDPS_T9_4pc
+                + stats.PriestDPS_T10_2pc + stats.PriestDPS_T10_4pc
+                + stats.ManaRestoreFromBaseManaPPM + stats.BonusSpellCritMultiplier
+                + stats.ManaRestore + stats.SpellsManaReduction + stats.HighestStat
+                + stats.ArcaneDamage + stats.FireDamage + stats.FrostDamage
+                + stats.HolyDamage + stats.NatureDamage + stats.ShadowDamage
+                + stats.ValkyrDamage
+
+                /*+ stats.SpellPowerFor15SecOnUse90Sec
+                + stats.SpellPowerFor15SecOnUse2Min + stats.SpellPowerFor20SecOnUse2Min
+                + stats.HasteRatingFor20SecOnUse2Min + stats.HasteRatingFor20SecOnUse5Min
+                + stats.SpellPowerFor10SecOnCast_15_45 + stats.SpellPowerFor10SecOnHit_10_45
+                + stats.SpellHasteFor10SecOnCast_10_45 + stats.TimbalsProc
+                + stats.PendulumOfTelluricCurrentsProc + stats.ExtractOfNecromanticPowerProc*/
+            ) > 0;
+            #endregion
+            #region Maybe
+            bool Maybe = (
+                stats.Stamina + stats.Health + stats.Resilience
+                + stats.Armor + stats.BonusArmor + stats.Agility
+                + stats.ArcaneResistance + stats.ArcaneResistanceBuff
+                + stats.FireResistance + stats.FireResistanceBuff
+                + stats.FrostResistance + stats.FrostResistanceBuff
+                + stats.NatureResistance + stats.NatureResistanceBuff
+                + stats.ShadowResistance + stats.ShadowResistanceBuff
+            ) > 0;
+            #endregion
+            #region No
+            bool No = (
+                stats.Strength + stats.AttackPower
+                + stats.ArmorPenetration + stats.ArmorPenetrationRating
+                + stats.TargetArmorReduction
+                + stats.ExpertiseRating
+                + stats.Dodge + stats.DodgeRating
+                + stats.Parry + stats.ParryRating
+                + stats.Defense + stats.DefenseRating
+            ) > 0;
+            #endregion
+            return Yes || (Maybe && !No);
         }
 
     }
