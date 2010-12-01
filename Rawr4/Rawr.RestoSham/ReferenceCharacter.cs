94a315cd901d5d14c99587117d9454fc6759a824
alpineman_cp <1a479dcfc433296634ff40aa24ea77234237a16fCC24nPzt>
2010-12-01 04:09:18 +0000

    Rawr4.RestoSham: - Beginning to implement a new modeling setup that should make future modifications much easier.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C55812

--- Rawr4/Rawr.RestoSham/ReferenceCharacter.cs
+++ Rawr4/Rawr.RestoSham/ReferenceCharacter.cs
@@ -10,7 +10,7 @@ namespace Rawr.RestoSham
         private Stats _RaceStats = null;
         private Stats _ItemStats = null;
         private Stats _BuffStats = null;
-        private Stats _TotalStats { get { return _RaceStats + _ItemStats + _BuffStats;}}
+        private Stats _TotalStats = null;
         private List<Spell> _AvailableSpells = new List<Spell>();
 
         internal ReferenceCharacter(CalculationsRestoSham calcs, Character character)
@@ -23,21 +23,24 @@ namespace Rawr.RestoSham
         {
             _AvailableSpells.Clear();
 
-            if (character.Level >= 83)
+            HealingRain healingRain = new HealingRain()
             {
-                HealingRain healingRain = new HealingRain()
-                {
-                    BaseManaCost = (int)(0.46 * _BaseMana)
-                };
-                _AvailableSpells.Add(healingRain);
-            }
+                BaseManaCost = (int)(0.46 * _BaseMana),
+                CostScale = 1f - character.ShamanTalents.TidalFocus * .02f,
+                EffectModifier = 1.1f + (character.ShamanTalents.SparkOfLife * .02f),
+                BonusSpellPower = 1 * ((1 + character.ShamanTalents.ElementalWeapons * .2f) * 150f)
+            };
+            _AvailableSpells.Add(healingRain);
 
             if (character.ShamanTalents.Riptide > 0)
             {
                 Riptide riptide = new Riptide()
                 {
                     BaseManaCost = (int)(0.10 * _BaseMana),
-                    HotDuration = 15f + (character.ShamanTalents.GlyphofRiptide ? 6f : 0f)
+                    HotDuration = 15f + (character.ShamanTalents.GlyphofRiptide ? 6f : 0f),
+                    CostScale = 1f - character.ShamanTalents.TidalFocus * .02f,
+                    EffectModifier = 1.1f + (character.ShamanTalents.SparkOfLife * .02f),
+                    BonusSpellPower = 1 * ((1 + character.ShamanTalents.ElementalWeapons * .2f) * 150f)
                 };
                 _AvailableSpells.Add(riptide);
             }
@@ -45,46 +48,81 @@ namespace Rawr.RestoSham
             ChainHeal chainHeal = new ChainHeal()
             {
                 BaseManaCost = (int)(0.17 * _BaseMana),
-                ChainedHeal = (_TotalStats.RestoSham4T10 > 0f)
+                ChainedHeal = (_TotalStats.RestoSham4T10 > 0f),
+                CostScale = 1f - character.ShamanTalents.TidalFocus * .02f,
+                EffectModifier = 1.1f + (character.ShamanTalents.SparkOfLife * .02f),
+                BonusSpellPower = 1 * ((1 + character.ShamanTalents.ElementalWeapons * .2f) * 150f)
             };
             _AvailableSpells.Add(chainHeal);
 
             HealingSpell healingSurge = new HealingSpell()
             {
+                SpellName = "Healing Surge",
                 BaseManaCost = (int)(0.27 * _BaseMana),
                 Cooldown = 0f,
                 BaseCastTime = 1.5f,
                 BaseCoefficient = 1.5f / 3.5f,
+                BaseEffect = 6004,
+                CostScale = 1f - character.ShamanTalents.TidalFocus * .02f,
+                EffectModifier = 1.1f + (character.ShamanTalents.SparkOfLife * .02f),
+                BonusSpellPower = 1 * ((1 + character.ShamanTalents.ElementalWeapons * .2f) * 150f)
             };
             _AvailableSpells.Add(healingSurge);
 
             HealingSpell healingWave = new HealingSpell()
             {
+                SpellName = "Healing Wave",
                 BaseManaCost = (int)(0.09 * _BaseMana),
                 Cooldown = 0f,
                 BaseCastTime = 3f,
                 BaseCoefficient = 3f / 3.5f,
+                BaseEffect = 3002,
+                CostScale = 1f - character.ShamanTalents.TidalFocus * .02f,
+                EffectModifier = 1.1f + (character.ShamanTalents.SparkOfLife * .02f),
+                BonusSpellPower = 1 * ((1 + character.ShamanTalents.ElementalWeapons * .2f) * 150f),
+                CastTimeReduction = 0.5f // Purification
             };
             _AvailableSpells.Add(healingWave);
 
             HealingSpell greaterHw = new HealingSpell()
             {
+                SpellName = "Greater Healing Wave",
                 BaseManaCost = (int)(0.3 * _BaseMana),
                 Cooldown = 0f,
                 BaseCastTime = 3f,
                 BaseCoefficient = 3f / 3.5f,
+                BaseEffect = 8005,
+                CostScale = 1f - character.ShamanTalents.TidalFocus * .02f,
+                EffectModifier = 1.1f + (character.ShamanTalents.SparkOfLife * .02f),
+                BonusSpellPower = 1 * ((1 + character.ShamanTalents.ElementalWeapons * .2f) * 150f),
+                CastTimeReduction = 0.5f // Purification
             };
             _AvailableSpells.Add(greaterHw);
         }
 
         internal void FullCalculate(Character character, Item additionalItem)
         {
-            _BuffStats = _Calculations.GetBuffsStats(character.ActiveBuffs);
-            _ItemStats = _Calculations.GetItemStats(character, additionalItem);
+            GetStats(character, additionalItem);
             GenerateSpellList(character);
+            CalculateSpells();
         }
 
         internal void IncrementalCalculate(Character character, Item additionalItem)
+        {
+            GetStats(character, additionalItem);
+            CalculateSpells();
+        }
+
+        private void GetStats(Character character, Item additionalItem)
+        {
+            _BuffStats = _Calculations.GetBuffsStats(character.ActiveBuffs);
+            _ItemStats = _Calculations.GetItemStats(character, additionalItem);
+            _TotalStats = _RaceStats + _ItemStats + _BuffStats;
+            _TotalStats.SpellPower += _TotalStats.Intellect - 10f;
+            _TotalStats.SpellHaste = (1 + StatConversion.GetSpellHasteFromRating(_TotalStats.HasteRating)) * (1 + _TotalStats.SpellHaste) - 1;
+        }
+
+        private void CalculateSpells()
         {
         }
     }
--- Rawr4/Rawr.RestoSham/RestoShamanSpells.cs
+++ Rawr4/Rawr.RestoSham/RestoShamanSpells.cs
@@ -5,16 +5,22 @@ namespace Rawr.RestoSham
     {
         public const float HealingPowerMultiplier = 1.88f;
 
+        public float BonusSpellPower { get; set; }
         public string SpellName { get; set; }
         public int BaseManaCost { get; set; }
         public float BaseCoefficient { get; set; }
         public float Cooldown { get; set; }
+        public float BaseEffect { get; set; }
+        public float CastTimeReduction { get; set; }
+        public float CostScale { get; set; }
+        public float EffectModifier { get; set; }
     }
     public class Hot : Spell
     {
         public float HotDuration { get; set; }
         public float BaseHotTickFrequency { get; set; }
         public float BaseHotCoefficient { get; set; }
+        public float BaseHotEffect { get; set; }
     }
     public sealed class HealingRain : Hot
     {
@@ -27,6 +33,8 @@ namespace Rawr.RestoSham
             Cooldown = 10f;
             BaseCastTime = 2f;
             BaseHotCoefficient = 0.5f;
+            BaseEffect = 0f;
+            BaseHotEffect = 3775;
         }
     }
     public class HealingSpell : Spell
@@ -43,6 +51,8 @@ namespace Rawr.RestoSham
             BaseHotTickFrequency = 3f;
             BaseCoefficient = 1.5f / 3.5f;
             BaseHotCoefficient = 0.5f;
+            BaseEffect = 2363;
+            BaseHotEffect = 3725;
         }
     }
     public sealed class ChainHeal : HealingSpell
@@ -55,6 +65,7 @@ namespace Rawr.RestoSham
             Cooldown = 0f;
             BaseCoefficient = 2.5f / 3.5f;
             BaseCastTime = 2.5f;
+            BaseEffect = 3178;
         }
     }
 }
