027fcbc841f6a8c72251814c87620ccf782fc290
Levva_cp <14064c3e782a7146c456bfa83e60e17ffb159ee62zdfWKTm>
2010-12-03 20:19:42 +0000

    Rawr: - (Addon) Add missing Locales files
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C55889

---
+++ Rawr4/Rawr.Addon/trunk/Rawr/Locales/Locale-enUS.lua
@@ -0,0 +1,40 @@
+
+local L = LibStub("AceLocale-3.0"):NewLocale("Rawr", "enUS", true);
+if not L then return end
+
+L["About"] = true
+L["Version"] = true 
+L["__URL__"] = "http://wow.curseforge.com/addons/rawr-official/"
+
+-------------------------------------------------------------------------------------
+-- Compulsory translation section - if these are not translated addon will not work
+-------------------------------------------------------------------------------------
+
+
+------------------
+-- Headers section
+------------------
+
+L["Configure Options"] = true
+L["Debug mode"] = true
+L["Open Export Window"] = true
+L["Keybind Title"] = "Rawr Keybinds"
+
+------------------
+-- Help section
+------------------
+
+L["help"] = "All options can be configured more easily using the Blizzard options menu. Press ESC and select addons then click on Rawr to configure."
+L["help1"] = "All options can be configured more easily using the Blizzard options menu."
+L["help2"] = "Press ESC and select addons then click on Rawr to configure."
+L["help_debug"] = "Activate or deactivate debug messages" 
+L["help_config"] = "Display easy to use graphical config panel"
+L["help_version"] = "Show version information" 
+L["help_open"] = "Open Export to Rawr Window"
+
+------------------
+-- Config section
+------------------
+
+L["config_debug_on"] = "Debug info will now be displayed"
+L["config_debug_off"] = "Debug info will NOT now be displayed"
