926728cb0e6cfafb41337d8a4ad8c48ab96bc57e
Jothay_cp <89f07ab5a0412695c4dbd13d89fded8c68adf163nkKyjNSt>
2010-12-20 10:12:12 +0000

    Last commit had a bug, fixed
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C56316

--- Rawr4/Rawr.Base/RawrAddonData.cs
+++ Rawr4/Rawr.Base/RawrAddonData.cs
@@ -95,11 +95,7 @@ namespace Rawr
                 #endregion
 
                 #region Filter the Bags & Bank lists for items you can't equip
-                List<Item> relevantItems = new List<Item>();
-                foreach (CharacterSlot cs in Character.EquippableCharacterSlots)
-                {
-                    relevantItems.AddRange(character.GetRelevantItems(cs));
-                }
+                List<Item> relevantItems = new List<Item>(ItemCache.GetRelevantItems(character.CurrentCalculations, character.Race));
                 List<int> relevantItemIDs = new List<int>();
                 foreach (Item i in relevantItems) {
                     if (relevantItemIDs.Contains(i.Id)) { continue; }
