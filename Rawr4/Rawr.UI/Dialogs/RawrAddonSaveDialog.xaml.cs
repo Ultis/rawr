5b417b30d1cb71facb3f0975bcfd6b30129a9a08
Levva_cp <14064c3e782a7146c456bfa83e60e17ffb159ee62zdfWKTm>
2010-12-23 13:52:25 +0000

    Rawr.UI : Add a Rawr.Addon Save dialog in preparation for being able to export optimised character to Rawr.addon.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C56434

---
+++ Rawr4/Rawr.UI/Dialogs/RawrAddonSaveDialog.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace Rawr.UI
+{
+    public partial class RawrAddonSaveDialog : ChildWindow
+    {
+        public RawrAddonSaveDialog(Character character)
+        {
+            InitializeComponent();
+            BuildExportLua(character);
+        }
+
+        private void BuildExportLua(Character character)
+        {
+            // this routine will build a LUA representation of the character for Rawr.Addon 
+            // and populate the textbox with that for cut and paste into addon
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+    }
+}
+
