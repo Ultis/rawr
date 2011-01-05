4e91cf7769fcc037d25ee2e021e216e8bc029040
Levva_cp <14064c3e782a7146c456bfa83e60e17ffb159ee62zdfWKTm>
2011-01-05 02:51:40 +0000

    Rawr.Addon:UI Prep work to use ItemSets for writing character & loaded character Prep work for exporting direct upgrades
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C56835

--- Rawr4/Rawr.UI/Character/RawrAddonSaveDialog.xaml.cs
+++ Rawr4/Rawr.UI/Character/RawrAddonSaveDialog.xaml.cs
@@ -18,7 +18,7 @@ namespace Rawr.UI
     public partial class RawrAddonSaveDialog : ChildWindow
     {
         private StringBuilder output;
-        private ComparisonCalculationBase[] DUCalcs = null;
+        private ComparisonCalculationBase[] DUCalcs = null; // holds direct upgrade calculations
         private static string nullItem = "item:0:0:0:0:0:0:0:0:0:0";
 
         public RawrAddonSaveDialog(Character character, ComparisonCalculationBase[] duCalcs)
@@ -28,12 +28,12 @@ namespace Rawr.UI
             {
                 output = new StringBuilder();
                 DUCalcs = duCalcs;
-                TB_XMLDump.Text = BuildExportLua(character, DUCalcs);
+                TB_XMLDump.Text = BuildExportLua(character);
                 TB_XMLDump.SelectAll();
             }
         }
 
-        private string BuildExportLua(Character character, ComparisonCalculationBase[] duCalcs)
+        private string BuildExportLua(Character character)
         {
             // this routine will build a LUA representation of the character for Rawr.Addon 
             // and populate the textbox with that for cut and paste into addon
@@ -42,33 +42,57 @@ namespace Rawr.UI
                 WriteLine(4, "realm = \"" + character.Realm + "\",");
                 WriteLine(4, "name = \"" + character.Name + "\",");
                 WriteSubPointTypes(4);
-                WriteLine(4, "character = {");
-                    WriteLine(8, "items = {");
-                        WriteItem(12, 1, character, CharacterSlot.Head);  
-                        WriteItem(12, 2, character, CharacterSlot.Neck);  
-                        WriteItem(12, 3, character, CharacterSlot.Shoulders); 
-                        WriteItem(12, 4, character, CharacterSlot.Shirt); 
-                        WriteItem(12, 5, character, CharacterSlot.Chest); 
-                        WriteItem(12, 6, character, CharacterSlot.Waist); 
-                        WriteItem(12, 7, character, CharacterSlot.Legs);  
-                        WriteItem(12, 8, character, CharacterSlot.Feet);
-                        WriteItem(12, 9, character, CharacterSlot.Wrist);
-                        WriteItem(12, 10, character, CharacterSlot.Hands);
-                        WriteItem(12, 11, character, CharacterSlot.Finger1);
-                        WriteItem(12, 12, character, CharacterSlot.Finger2);
-                        WriteItem(12, 13, character, CharacterSlot.Trinket1);
-                        WriteItem(12, 14, character, CharacterSlot.Trinket2);
-                        WriteItem(12, 15, character, CharacterSlot.Back);
-                        WriteItem(12, 16, character, CharacterSlot.MainHand);
-                        WriteItem(12, 17, character, CharacterSlot.OffHand);
-                        WriteItem(12, 18, character, CharacterSlot.Ranged);
-                        WriteItem(12, 19, character, CharacterSlot.Tabard);
-                    WriteLine(8, "},");
-                WriteLine(4, "},");
+                WriteCharacter("character", character); // replace with ItemSet itemSet = character.GetItemSetByName("Current");
+                WriteLoadedSet(character);
+                WriteDirectUpgrades();
             WriteLine(0, "}");
             return output.ToString();
         }
 
+        private void WriteCharacter(string block, Character character)
+        {
+            WriteLine(4, block + " = {");
+            WriteLine(8, "items = {");
+            WriteItem(12, 1, character, CharacterSlot.Head);
+            WriteItem(12, 2, character, CharacterSlot.Neck);
+            WriteItem(12, 3, character, CharacterSlot.Shoulders);
+            WriteItem(12, 4, character, CharacterSlot.Shirt);
+            WriteItem(12, 5, character, CharacterSlot.Chest);
+            WriteItem(12, 6, character, CharacterSlot.Waist);
+            WriteItem(12, 7, character, CharacterSlot.Legs);
+            WriteItem(12, 8, character, CharacterSlot.Feet);
+            WriteItem(12, 9, character, CharacterSlot.Wrist);
+            WriteItem(12, 10, character, CharacterSlot.Hands);
+            WriteItem(12, 11, character, CharacterSlot.Finger1);
+            WriteItem(12, 12, character, CharacterSlot.Finger2);
+            WriteItem(12, 13, character, CharacterSlot.Trinket1);
+            WriteItem(12, 14, character, CharacterSlot.Trinket2);
+            WriteItem(12, 15, character, CharacterSlot.Back);
+            WriteItem(12, 16, character, CharacterSlot.MainHand);
+            WriteItem(12, 17, character, CharacterSlot.OffHand);
+            WriteItem(12, 18, character, CharacterSlot.Ranged);
+            WriteItem(12, 19, character, CharacterSlot.Tabard);
+            WriteLine(8, "},");
+            WriteLine(4, "},");
+        }
+
+        private void WriteLoadedSet(Character character)
+        {
+            // ItemSet itemSet = character.GetItemSetByName("Last Loaded Set");
+        }
+
+        private void WriteDirectUpgrades()
+        {
+            if (DUCalcs == null)
+            {
+                WriteLine(4, "upgrades = {}, ");
+                return;
+            }
+            WriteLine(4, "upgrades = {");
+
+            WriteLine(4, "}, ");
+        }
+  
         private void WriteLine(int indent, string text)
         {
             output.Append(' ', indent);
