9aa372d3c112c531468ed6a0674ce529f19c129d
rigamonk_cp <5e225fa5261274e042d22171309d9b6faedfa765vMQk2z2S>
2011-01-18 03:53:57 +0000

    Split out active and passive abilities into separate classes
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C57288

--- Rawr4/Rawr.Retribution/PaladinConstants.cs
+++ Rawr4/Rawr.Retribution/PaladinConstants.cs
@@ -3,93 +3,112 @@ namespace Rawr.Retribution
 {
     public class PaladinConstants
     {
-        #region Damage Base, Coefficients and bonuses
-
-        /* DMG consts are additive */
-        /// <summary>
-        /// Average of base damage from exorcism
-        /// </summary>
-        internal const float CONS_DMG = 750;
-        /* BONUS consts are multiplicative */
-        /// <summary>
-        /// Base damage bonus from consecration
-        /// </summary>
-        internal const float CS_DMG_BONUS = 1.15f;
-        /// <summary>
-        /// damage bonus coefficient added directly to exorcism strike
-        /// </summary>
-        internal const float EXO_DMG_BONUS = .344f;
-        /// <summary>
-        /// damage bonus coefficient for Templars Verdict
-        /// </summary>
-        internal const float TV_DMG_BONUS = 2.35f;
+        #region Auto Attack
+        internal const float AUTO_COOLDOWN = 0;
         #endregion
 
-        #region Base cooldown duration Values
-        /// <summary>
-        /// Crusader Strike cooldown (sec)
-        /// </summary>
-        internal const float CS_COOLDOWN = 4.5f;
+        #region Consecration
+        internal const int CONS_ID = 38385;
         /// <summary>
         /// Consecration cooldown (sec)
         /// </summary>
-        internal const float CONS_COOLDOWN = 0;
+        internal const float CONS_COOLDOWN = 30;
         /// <summary>
-        /// Exorcism cooldown (sec)
+        /// Consecration base damage/second
         /// </summary>
+        internal const float CONS_BASE_DMG = 810;
+        internal const float CONS_COEFF = .26f;
+        internal const float CONS_DEFAULT_DURATION = 10;
+        #endregion
+
+        #region Crusader Strike
+        internal const int CS_ID = 35395;
+        internal const float CS_COOLDOWN = 4.5f;
+        internal const float CS_DMG_BONUS = 1.15f;
+        internal const float CS_COEFF = 3.3f;
+        #endregion
+
+        #region Exorcism
+        internal const int EXO_ID = 879;
         internal const float EXO_COOLDOWN = 0;
-        /// <summary>
-        /// Hammer of Wrath cooldown (sec)
-        /// </summary>
+        internal const float EXO_DMG_BONUS = .344f;
+        internal const float EXO_AVG_DMG = 2741;
+
+        #endregion
+
+        #region Hammer Of Wrath
+        internal const int HOW_ID = 24275;
         internal const float HOW_COOLDOWN = 6;
+        internal const int HOW_AVG_DMG = 4015;
+        #endregion
+
+        #region Holy Wrath
         /// <summary>
-        /// Auto-Attack cooldown (sec)
+        /// Holy Wrath
         /// </summary>
-        internal const float AUTO_COOLDOWN = 0;
+        internal const int HOLY_WRATH_ID = 2812;
         /// <summary>
         /// Holy Wrath cooldown (sec)
         /// </summary>
-        internal const float HW_COOLDOWN = 16;
+        internal const float HOLY_WRATH_COOLDOWN = 16;
+        /// <summary>
+        /// Holy Wrath base damage
+        /// </summary>
+        internal const int HOLY_WRATH_BASE_DMG = 2435;
+        /// <summary>
+        /// used for calculating % of holy power applied
+        /// </summary>
+        internal const float HOLY_WRATH_COEFF = .61f;
         #endregion
 
+        #region Seal Of Truth and Judgement Of Truth
         /// <summary>
-        /// Spell ID's assigned by Blizzard
+        /// Seal Of Truth ID
         /// </summary>
-        public enum SpellIds
-        {
-            /// <summary>
-            /// Auto attack
-            /// </summary>
-            AUTO_ID,
-            /// <summary>
-            /// Crusader strike
-            /// </summary>
-            CS_ID = 35395,
-            /// <summary>
-            /// Exorcism
-            /// </summary>
-            EXO_ID = 879,
-            /// <summary>
-            /// Seal Of Truth
-            /// </summary>
-            SOT_ID,
-            /// <summary>
-            /// Templars Verdict
-            /// </summary>
-            TV_ID = 63867,
-            /// <summary>
-            /// Seal of Righteousness
-            /// </summary>
-            SOR_ID = 20154,
-            /// <summary>
-            /// Hammer Of Wrath
-            /// </summary>
-            HOW_ID = 24275,
-            /// <summary>
-            /// Holy Wrath
-            /// </summary>
-            HW_ID = 2812,
-            CONS_ID = 38385
-        }
+        internal const int SOT_ID = 31801;
+        internal const float SOT_DMG = 0;
+        internal const int JOT_ID = 31804;
+        #endregion
+
+        #region Seal of Justice and Judgement of Justice
+        internal const int SOJ_ID = 20164;
+        internal const float SOJ_AP_COEFF = .005f;
+        internal const float SOJ_HOLY_COEFF = .01f;
+        #endregion
+
+        #region Judgement Of Insight
+        internal const int JOI_ID = 20165;
+        internal const float JOI_AP_COEFF = .16f;
+        internal const float JOI_HOLY_COEFF = .25f;
+        #endregion
+
+        #region Templars Verdict
+        /// <summary>
+        /// Templars Verdict
+        /// </summary>
+        internal const int TV_ID = 85256;
+        internal const float TV_ONE_STK = .3f;
+        internal const float TV_TWO_STK = .9f;
+        internal const float TV_THREE_STK = 2.35f;
+
+        #endregion
+
+        #region Sheath of Light
+        internal const float SHEATH_AP_COEFF = .3f;
+        internal const float SHEATH_SPHIT_COEFF = .08f;
+        #endregion
+
+        #region Avenging Wrath
+        internal const int AW_ID = 31884;
+        internal const float AW_COOLDOWN = 180;
+        internal const float AW_DURATION = 20f;
+        internal const float AW_DMG_BONUS = .2f;
+        #endregion
+
+        #region Two Handed Weapon Specialization
+        internal const float TWO_H_SPEC = .2f;
+        #endregion
+
+        internal const float JUDGE_COOLDOWN = 8;
     }
 }
---
+++ Rawr4/Rawr.Retribution/PassiveAbility.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rawr.Retribution.WPF
+{
+
+    public abstract class PassiveAbilityBase
+    {
+        public abstract float? CritBonus { get; }
+        public abstract float? MeleeHitBonus { get; }
+        public abstract float? SpellHitBonus { get; }
+        public abstract float? DamageBonus { get; }
+        public abstract float? SpellPowerBonus { get; }
+        public abstract float? AttackPowerBonus { get; }
+    }
+    public class SheathOfLight : PassiveAbilityBase
+    {
+        public override float? CritBonus { get { return null; } }
+        public override float? MeleeHitBonus { get { return null; } }
+        public override float? AttackPowerBonus { get { return null; } }
+        public override float? DamageBonus { get { return null; } }
+        /// <summary>
+        /// increase to spell hit - multiplicative
+        /// </summary>
+        public override float? SpellHitBonus
+        {
+            get { return PallyConstants.SHEATH_SPHIT_COEFF; }
+        }
+        /// <summary>
+        /// increase to spellpower - additive
+        /// </summary>
+        public override float? SpellPowerBonus
+        {
+            get { return CurrentState.AttackPower * PaladinConstants.SHEATH_AP_COEFF; }
+        }
+    }
+    public class TwoHSpecilization : PassiveAbilityBase
+    {
+        public override float? CritBonus { get { return null; } }
+        public override float? MeleeHitBonus { get { return null; } }
+        public override float? SpellHitBonus { get { return null; } }
+        public override float? SpellPowerBonus { get { return null; } }
+        public override float? AttackPowerBonus { get { return null; } }
+        /// <summary>
+        /// Bonus added to melee strike - Additive
+        /// </summary>
+        public override float? DamageBonus
+        {
+            get { return CurrentState.WeaponDamage * PaladinConstants.TWO_H_SPEC; }
+        }
+    }
+    public class HandOfLight : PassiveAbilityBase
+    {
+        /**Your autoattacks have a 8% chance to grant Hand of Light, causing your next Holy Power ability to consume
+         * no Holy Power and to cast as if 3 Holy Power were consumed.  Each point of Mastery increases the chance by an additional 1%. **/
+        public override float? CritBonus
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override float? MeleeHitBonus
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override float? SpellHitBonus
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override float? DamageBonus
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override float? SpellPowerBonus
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override float? AttackPowerBonus
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
