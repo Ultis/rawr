061c4adc08233013afd554ba0863b7dc3888ec0a
Shazear_cp <10c9ac5865e763141149f2b1a65e9add4360a2659J3JrJxD>
2011-02-03 21:43:12 +0000

    Rawr.Base: - Fix for Defect 19746: Striping out hyphens from the server names that have it when making a character request.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C57749

--- Rawr4/Rawr.Base/Rawr4ArmoryService.cs
+++ Rawr4/Rawr.Base/Rawr4ArmoryService.cs
@@ -187,7 +187,9 @@ namespace Rawr
             _lastName = name;
             //_canceled = false;
             //_lastRequestWasItem = false;
-            string url = string.Format(URL_CHAR_REQ, UrlEncode(name), region.ToString().ToLower(), UrlEncode(realm) /*, forceRefresh ? "1" : "0"*/);
+            string szRealmCleaned = realm.Replace("-", "");
+            string url = string.Format(URL_CHAR_REQ, UrlEncode(name), region.ToString().ToLower(), UrlEncode(szRealmCleaned) /*, forceRefresh ? "1" : "0"*/);
+            // Defect 19746: Server names that have "-" are not actually using them in their URL.
             _webClient.DownloadStringAsync(new Uri(url));
             this.Progress = "Downloading Character Data...";
         }
