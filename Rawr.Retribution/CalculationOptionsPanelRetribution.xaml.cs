b429c25d2e1b44584045cde7ffbeb190618f3d86
Jothay_cp <89f07ab5a0412695c4dbd13d89fded8c68adf163nkKyjNSt>
2011-03-09 21:14:26 +0000

    Rawr.Retribution: Fixed Ret's options pane constructions back to the standard for all models, the DataContext stuff wasn't being assigned correctly.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C58588

--- Rawr.Retribution/CalculationOptionsPanelRetribution.xaml.cs
+++ Rawr.Retribution/CalculationOptionsPanelRetribution.xaml.cs
@@ -15,47 +15,95 @@ namespace Rawr.Retribution
 {
     public partial class CalculationOptionsPanelRetribution : ICalculationOptionsPanel
     {
-        public CalculationOptionsPanelRetribution()
-        {
-            InitializeComponent();
-        }
+        public bool _loadingCalculationOptions = false;
 
         public UserControl PanelControl { get { return this; } }
 
         #region ICalculationOptionsPanel Members
-        private CalculationOptionsRetribution CalcOpts
-        {
-            get { return (CalculationOptionsRetribution) character.CalculationOptions; }
-        }
+        CalculationOptionsRetribution calcOpts = null;
 
         private Character character;
         public Character Character
         {
-            get
-            {
-                return character;
-            }
-            set
-            {
-                if (character != null && character.CalculationOptions != null && character.CalculationOptions is CalculationOptionsRetribution)
-                    ((CalculationOptionsRetribution)character.CalculationOptions).PropertyChanged -= new PropertyChangedEventHandler(CalculationOptionsRetribution_PropertyChanged);
+            get { return character; }
+            set {
+                // Kill any old event connections
+                if (character != null && character.CalculationOptions != null
+                    && character.CalculationOptions is CalculationOptionsRetribution)
+                    ((CalculationOptionsRetribution)character.CalculationOptions).PropertyChanged
+                        -= new PropertyChangedEventHandler(CalculationOptionsRetribution_PropertyChanged);
+                // Apply the new character
                 character = value;
-                if (character.CalculationOptions == null)
-                    character.CalculationOptions = new CalculationOptionsRetribution();
-
-                CalcOpts.PropertyChanged += new PropertyChangedEventHandler(CalculationOptionsRetribution_PropertyChanged);
+                // Load the new CalcOpts
+                LoadCalculationOptions();
+                // Model Specific Code
+                // Set the Data Context
+                LayoutRoot.DataContext = calcOpts;
+                // Add new event connections
+                calcOpts.PropertyChanged += new PropertyChangedEventHandler(CalculationOptionsRetribution_PropertyChanged);
+                // Run it once for any special UI config checks
                 CalculationOptionsRetribution_PropertyChanged(null, new PropertyChangedEventArgs(""));
             }
         }
-
+        public CalculationOptionsPanelRetribution() {
+            _loadingCalculationOptions = true;
+            try {
+                InitializeComponent();
+                // Model Specific Code
+                // n/a
+            } catch (Exception ex) {
+                new Base.ErrorBox() {
+                    Title = "Error in creating the Retribution Options Pane",
+                    TheException = ex,
+                    Function = "CalculationOptionsPanelRetribution()",
+                }.Show();
+            }
+            _loadingCalculationOptions = false;
+        }
+        public void LoadCalculationOptions()
+        {
+            string info = "";
+            _loadingCalculationOptions = true;
+            try {
+                if (Character != null && Character.CalculationOptions == null)
+                {
+                    // If it's broke, make a new one with the defaults
+                    Character.CalculationOptions = new CalculationOptionsRetribution();
+                    _loadingCalculationOptions = true;
+                }
+                else if (Character == null) { return; }
+                calcOpts = Character.CalculationOptions as CalculationOptionsRetribution;
+                // == Model Specific Code ==
+            } catch (Exception ex) {
+                new Base.ErrorBox() {
+                    Title = "Error in loading the Retribution Options Pane",
+                    Function = "LoadCalculationOptions()",
+                    TheException = ex,
+                    Info = info,
+                }.Show();
+            }
+            _loadingCalculationOptions = false;
+        }
         void CalculationOptionsRetribution_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            Character.OnCalculationsInvalidated();
+            if (_loadingCalculationOptions) { return; }
+            if (e == null) { e = new PropertyChangedEventArgs(""); }
+            // This would handle any special changes, especially combobox assignments, but not when the pane is trying to load
+            if (e.PropertyName.Contains("SG_")
+                || e.PropertyName == "StatsList"
+                || e.PropertyName == "StatsIncrement"
+                || e.PropertyName == "CalculationToGraph")
+            {
+                // These fields shouldn't be forcing new calcs on each click
+                return;
+            }
+            //
+            if (Character != null) { Character.OnCalculationsInvalidated(); }
         }
 
         private void btnResetBelow20_Click(object sender, RoutedEventArgs e)
         {
-            CalcOpts.TimeUnder20 = .18f;
+            calcOpts.TimeUnder20 = .18f;
         }
         #endregion
 
