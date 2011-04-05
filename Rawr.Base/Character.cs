70360c966c5887afeefaa1a248874602808803b7
Jothay_cp <89f07ab5a0412695c4dbd13d89fded8c68adf163nkKyjNSt>
2011-04-05 18:14:36 +0000

    Rawr.Base: Fix for Issue 20418: * 'T11_0' is not a valid value for TierLevels error on character file load - Had updated the other possible values but forgot T11
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS03]$/Rawr;C59330

--- Rawr.Base/Character.cs
+++ Rawr.Base/Character.cs
@@ -3174,6 +3174,7 @@ namespace Rawr
                         .Replace("T7_0", "T11_10").Replace("T8_0", "T11_10").Replace("T9_0", "T11_10").Replace("T10_0", "T11_10")
                         .Replace("T7_5", "T11_10").Replace("T8_5", "T11_10").Replace("T9_5", "T11_10").Replace("T10_5", "T11_10")
                         .Replace("T7_9", "T11_10").Replace("T8_9", "T11_10").Replace("T9_9", "T11_10").Replace("T10_9", "T11_10")
+                        .Replace("T11_0", "T11_10").Replace("T11_5", "T11_10").Replace("T11_9", "T11_10")
                         .Replace("<Attack xsi:type=\"DoT\">", "<Attack>");
 
                     if (xml.IndexOf("<CalculationOptions>") != xml.LastIndexOf("<CalculationOptions>"))
